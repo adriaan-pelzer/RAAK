88d49fd3a94124db9b2b98500c5360b5ed98ef6b
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2011-12-07 13:32:05 +0200

    changing doctype to html 5 doctype

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE>
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
 <head profile="http://gmpg.org/xfn/11">
     <title>RAAK | Digital &amp; Social Media Agency London - Putting you in touch with your crowds</title>
