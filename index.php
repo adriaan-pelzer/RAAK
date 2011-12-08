ade94e093e46c76f969dd8095aafb8613c62624b
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2011-12-08 16:29:58 +0200

    adding hr back in and minor adjustments to header bottom padding and bluebox upper padding

--- index.php
+++ index.php
@@ -304,7 +304,8 @@ function next () {
             </section><!-- #logox_counter -->
             <section id="bluebox_home_left" class="bluebox rounded-corners">
                 <h3 class="din-schrift"><a href="http://wewillraakyou.com/our-work/">Our Work</a></h3>
-                    <p>RAAK is a social &amp; digital media <em>plug-in</em>. We put you in touch with people, be it your customers, your suppliers, or employees.</p>
+                <hr />    
+                <p>RAAK is a social &amp; digital media <em>plug-in</em>. We put you in touch with people, be it your customers, your suppliers, or employees.</p>
             </section><!-- #bluebox_home_left -->
             <section id="bluebox_home_right" class="bluebox rounded-corners">
                 <div id="bluebox_top"></div>
