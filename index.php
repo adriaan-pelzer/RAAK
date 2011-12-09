0ad5789bed586297b1ea5f7f37cc11178e947f9c
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2011-12-09 11:40:26 +0200

    removing lower margin on .bluebox_content_link and adding a space between more projects and downward arrow

--- index.php
+++ index.php
@@ -373,7 +373,7 @@ function next () {
                                 <span class="overview">A strategy & functional specification for a social network for the world's most influential people. <br \><strong>Read More >></strong></span>
                             </li>
                             <li class="bluebox_body_content_link">
-                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects&#9660;</a>
+                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
                             </li>
                         </ul>
                     </section>
