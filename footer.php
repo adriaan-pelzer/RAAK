ce5feafd6fc37e9ebbcfeb2a00f90f3be3e6eff3
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2011-12-15 14:16:18 +0200

    creating footer and header and sidebar files

---
+++ footer.php
@@ -0,0 +1,30 @@
+    <footer>
+        <span class="footer_copyright">&copy; RAAK 2011</span>
+        <a title="RAAK.it URL shortener" href="http://raak.it">RAAK.it</a>
+        <span class="seperator">|</span>
+        <a rel="nofollow" href="http://www.youtube.com/user/wewillRAAKyou">RAAK on Youtube</a>
+        <span class="seperator">|</span>
+        <a rel="nofollow" href="http://www.twitter.com/RAAKonteurs">RAAK on Twitter</a>
+        <span class="seperator">|</span>
+        <a rel="nofollow" href="http://www.linkedin.com/companies/raak">RAAK on LinkedIn</a>
+        <span class="seperator">|</span>
+        <a rel="nofollow" href="http://www.facebook.com/pages/RAAK/167105176615">RAAK on Facebook</a>
+        <!--span class="footer_seperator">|</span>
+        <span class="footer_link"><a href="http://www.flickr.com">RAAK on Flickr</a></span-->
+    </footer><!-- #footer -->
+</div><!-- #wrapper .hfeed -->
+
+<script type='text/javascript' src='http://wewillraakyou.com/wp-includes/js/jquery/jquery.form.js?ver=2.02m'></script>
+<script type='text/javascript' src='http://wewillraakyou.com/wp-content/plugins/contact-form-7/scripts.js?ver=2.3.1'></script>
+<!--stats_footer_test--><script src="http://stats.wordpress.com/e-201149.js" type="text/javascript"></script>
+<script type="text/javascript">
+st_go({blog:'14895691',v:'ext',post:'4'});
+var load_cmc = function(){linktracker_init(14895691,4,2);};
+if ( typeof addLoadEvent != 'undefined' ) addLoadEvent(load_cmc);
+else load_cmc();
+</script>
+<script type="text/javascript" src="http://apis.google.com/js/plusone.js"></script>
+</body>
+</html>
+
+
---
+++ header.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head>
+    <title>RAAK | Digital &amp; Social Media Agency London - Putting you in touch with your crowds</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="google-site-verification" content="gopVig1vD9ASr_RMvJGPk5w2Rk9dQ16tJY4v7-5E5dc" />
+    <!--link rel="stylesheet" type="text/css" href="http://wewillraakyou.com/wp-content/themes/RAAK/style.css?ver=1.0" / -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo ("template_url"); ?>/css/style.css" />
+    <!--dynamic-cached-content--><link rel="stylesheet" href="http://wewillraakyou.com/wp-content/themes/RAAK/style_ch10.css?ver=1.0" /><!-- Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2 --><!--/dynamic-cached-content-->
+    <!--[if IE]>
+    <link rel="stylesheet" type="text/css" href="http://wewillraakyou.com/wp-content/themes/RAAK/style_ie.css" />
+    <![endif]-->
+    <!--[if IE 8]>
+    <link rel="stylesheet" type="text/css" href="http://wewillraakyou.com/wp-content/themes/RAAK/style_ie_8.css" />
+    <![endif]-->
+    <!--[if IE 7]>
+    <link rel="stylesheet" type="text/css" href="http://wewillraakyou.com/wp-content/themes/RAAK/style_ie_7.css" />
+    <![endif]-->
+<!--Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2-->
+
+<link rel="alternate" type="application/rss+xml" title="RAAK | Digital &amp; Social Media Agency London &raquo; Home Comments Feed" href="http://wewillraakyou.com/home/feed/" />
+<link rel='stylesheet' id='NextGEN-css'  href='http://wewillraakyou.com/wp-content/plugins/nextgen-gallery/css/nggallery.css?ver=1.0.0' type='text/css' media='screen' />
+<link rel='stylesheet' id='shutter-css'  href='http://wewillraakyou.com/wp-content/plugins/nextgen-gallery/shutter/shutter-reloaded.css?ver=1.3.0' type='text/css' media='screen' />
+<link rel='stylesheet' id='contact-form-7-css'  href='http://wewillraakyou.com/wp-content/plugins/contact-form-7/styles.css?ver=2.3.1' type='text/css' media='all' />
+<script type='text/javascript'>
+/* <![CDATA[ */
+var shutterSettings = {
+    msgLoading: "L O A D I N G",
+        msgClose: "Click to Close",
+        imageCount: "1"
+};
+/* ]]> */
+</script>
+<script type='text/javascript' src='http://wewillraakyou.com/wp-content/plugins/nextgen-gallery/shutter/shutter-reloaded.js?ver=1.3.0'></script>
+<script type='text/javascript' src='http://wewillraakyou.com/wp-includes/js/jquery/jquery.js?ver=1.4.2'></script>
+<link rel="EditURI" type="application/rsd+xml" title="RSD" href="http://wewillraakyou.com/xmlrpc.php?rsd" />
+<link rel="wlwmanifest" type="application/wlwmanifest+xml" href="http://wewillraakyou.com/wp-includes/wlwmanifest.xml" /> 
+<link rel='index' title='RAAK | Digital &amp; Social Media Agency London' href='http://wewillraakyou.com/' />
+<link rel='prev' title='Adriaan Pelzer' href='http://wewillraakyou.com/about/the-founders/adriaan-pelzer/' />
+<link rel='next' title='Blog Archive' href='http://wewillraakyou.com/blog-archive/' />
+<meta name="generator" content="WordPress 3.0.1" />
+<link rel='canonical' href='http://wewillraakyou.com/' />
+<link rel='shortlink' href='http://wp.me/P10v35-4' />
+
+<script type="text/javascript" src="http://wewillraakyou.com/wp-content/plugins/quicktimepost.js"></script>
+<script type="text/javascript">
+//<![CDATA[
+var _wpcf7 = { cached: 1 };
+//]]>
+</script>
+
+<!-- BEGIN: Open Graph Protocol Tools: http://opengraphprotocol.org/ for more info -->
+<!--meta property="og:title" content="Home" /-->
+<!--meta property="og:type" content="blog" /-->
+<!--meta property="og:image" content="http://wewillraakyou.com/wp-content/plugins/open-graph-protocol-tools/default.png" /-->
+<!--meta property="og:url" content="http://wewillraakyou.com/" /-->
+<!--meta property="og:site_name" content="RAAK | Digital &amp;amp; Social Media Agency London" /-->
+<!--meta property="fb:admins" content="544207149" /-->
+<!--meta property="fb:appid" content="101262553270969" /-->
+<!-- End: Open Graph Protocol Tools-->
+<link id='MediaRSS' rel='alternate' type='application/rss+xml' title='NextGEN Gallery RSS Feed' href='http://wewillraakyou.com/wp-content/plugins/nextgen-gallery/xml/media-rss.php' />
+
+<!-- NextGeEN Gallery CoolIris/PicLens support -->
+<script type="text/javascript" src="http://lite.piclens.com/current/piclens_optimized.js"></script>
+<!-- /NextGEN Gallery CoolIris/PicLens support -->
+<meta name='NextGEN' content='1.6.1' />
+    <link rel="alternate" type="application/rss+xml" href="http://wewillraakyou.com/feed/" title="RAAK | Digital &amp; Social Media Agency London latest posts" />
+    <link rel="alternate" type="application/rss+xml" href="http://wewillraakyou.com/comments/feed/" title="RAAK | Digital &amp; Social Media Agency London latest comments" />
+    <link rel="pingback" href="http://wewillraakyou.com/xmlrpc.php" />
+
+    <script type="text/javascript" src="http://wewillraakyou.com/wp-content/themes/RAAK/jquery.min.js"></script>
+<script type="text/javascript">
+<!--
+var active_logos = 'R';
+
+var logo_letters = new Array();
+logo_letters['R'] = new Array();
+logo_letters['R'][0] = new Image();
+logo_letters['R'][0].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/3326823e578cbe72c52ca50e5b338494.jpg&width=35&height=42";
+logo_letters['R'][1] = new Image();
+logo_letters['R'][1].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/a8b2a4f4c69564aa9f11381c0c260235.jpg&width=35&height=42";
+logo_letters['R'][2] = new Image();
+logo_letters['R'][2].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/2dcfb0448f8b53e4a913a16c8f3413f2.jpg&width=35&height=42";
+logo_letters['R'][3] = new Image();
+logo_letters['R'][3].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/7832f12fb6b6af543b029481f0b7baa4.png&width=35&height=42";
+logo_letters['R'][4] = new Image();
+logo_letters['R'][4].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/5196b17d8503c6ad7a7accfefbdeda83.jpg&width=35&height=42";
+logo_letters['R'][5] = new Image();
+logo_letters['R'][5].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/a5605d2e128aaa3779904d517d211942.png&width=35&height=42";
+logo_letters['R'][6] = new Image();
+logo_letters['R'][6].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/241eca044d005504eabcaa5d3e6b9068.jpg&width=35&height=42";
+logo_letters['R'][7] = new Image();
+logo_letters['R'][7].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/4bd891a62ea023f66c37176c84d4a7fa.png&width=35&height=42";
+logo_letters['A'] = new Array();
+logo_letters['A'][0] = new Image();
+logo_letters['A'][0].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/27027cdc0a51f42ce66c576cb4916fa1.png&width=35&height=42";
+logo_letters['A'][1] = new Image();
+logo_letters['A'][1].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/94f1befdbb9439a574a82458d1fffc06.jpg&width=35&height=42";
+logo_letters['A'][2] = new Image();
+logo_letters['A'][2].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/51de71bfb5dfe9edc1c73f0ca5a4408a.jpg&width=35&height=42";
+logo_letters['A'][3] = new Image();
+logo_letters['A'][3].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/61bc98b99c6340b6be2b151e47d08d28.jpg&width=35&height=42";
+logo_letters['A'][4] = new Image();
+logo_letters['A'][4].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/811f9f13a4e7294ee86ff2288d3534a7.jpg&width=35&height=42";
+logo_letters['A'][5] = new Image();
+logo_letters['A'][5].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/0387e51d17f01d524c0b07de1469eb6a.png&width=35&height=42";
+logo_letters['A'][6] = new Image();
+logo_letters['A'][6].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/45d36d1d36846e6a7210254b7c10b1e0.png&width=35&height=42";
+logo_letters['A'][7] = new Image();
+logo_letters['A'][7].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/eb9895b79c7e1cd240cb3960e16ad30b.jpg&width=35&height=42";
+logo_letters['A'][8] = new Image();
+logo_letters['A'][8].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/3fe9de8c3f0b3d7286c6f7c97afd46ba.jpg&width=35&height=42";
+logo_letters['A'][9] = new Image();
+logo_letters['A'][9].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/7bf62183cb8f5c938b5a0da6bf959647.jpg&width=35&height=42";
+logo_letters['A'][10] = new Image();
+logo_letters['A'][10].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/73e0fec8bb33c1cdc59c812d3954bbfb.jpg&width=35&height=42";
+logo_letters['K'] = new Array();
+logo_letters['K'][0] = new Image();
+logo_letters['K'][0].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/ba0c3f6be22d07916abf6ca1f7d37d8a.jpg&width=35&height=42";
+logo_letters['K'][1] = new Image();
+logo_letters['K'][1].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/c6a513db33554763c49386bed4673bc3.jpg&width=35&height=42";
+logo_letters['K'][2] = new Image();
+logo_letters['K'][2].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/1123dcb9de8cff45074fc0b8029b9751.jpg&width=35&height=42";
+logo_letters['K'][3] = new Image();
+logo_letters['K'][3].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/78ce302c784ece3f208eba8152c2bc58.jpg&width=35&height=42";
+logo_letters['K'][4] = new Image();
+logo_letters['K'][4].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/93455d62240dc78204038d89fcc4eaaa.png&width=35&height=42";
+logo_letters['K'][5] = new Image();
+logo_letters['K'][5].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/94f1cb321eb37b70e6f4a789514aee55.jpg&width=35&height=42";
+logo_letters['K'][6] = new Image();
+logo_letters['K'][6].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/0c71b1cbe12da4fdbe88df63f21dbfce.jpg&width=35&height=42";
+logo_letters['K'][7] = new Image();
+logo_letters['K'][7].src = "http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/adc3d69153a68c25153f5e7cafdb924c.png&width=35&height=42";
+function cycle_logo_letter (display_letter) {
+    switch (display_letter) {
+    case 'R':
+        var letter_id = 'logo_letter_r1';
+        var letter = 'R';
+        break;
+    case 'A1':
+        var letter_id = 'logo_letter_a2';
+        var letter = 'A';
+        break;
+    case 'A2':
+        var letter_id = 'logo_letter_a3';
+        var letter = 'A';
+        break;
+    case 'K':
+        var letter_id = 'logo_letter_k4';
+        var letter = 'K';
+        break;
+    }
+
+    $("#"+letter_id+" a img").fadeTo('slow', 0, function () {
+        if (logo_letters[letter].length != 0) {
+            $("#"+letter_id+" a img").attr('src', logo_letters[letter][Math.floor(Math.random()*logo_letters[letter].length)].src);
+        }
+
+        $("#"+letter_id+" a img").fadeTo('slow', 1);
+    });
+
+    setTimeout ("cycle_logo_letter('"+display_letter+"');", 5000 + Math.floor(Math.random()*5000));
+}
+$(document).ready( function () {
+    setTimeout ("cycle_logo_letter('R');", 5000 + Math.floor(Math.random()*5000));
+    setTimeout ("cycle_logo_letter('A1');", 5000 + Math.floor(Math.random()*5000));
+    setTimeout ("cycle_logo_letter('A2');", 5000 + Math.floor(Math.random()*5000));
+    setTimeout ("cycle_logo_letter('K');", 5000 + Math.floor(Math.random()*5000));
+});
+//-->
+</script>
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-350036-10']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+</head>
+
+<body class="wordpress y2011 m12 d07 h07 home page pageid-4 page-author-admin page-template page-template-home-php">
+<div id="wrapper" class="hfeed">
+    <header id="header" class="rounded-corners din-schrift">
+        <div id="blog-title">
+            <h1>RAAK</h1>
+            <div id="logo_letter_r1" class="logo_letter">
+                <a href="http://wewillraakyou.com/" title="RAAK | Digital &amp; Social Media Agency London" rel="home">
+                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/4bd891a62ea023f66c37176c84d4a7fa.png&width=35&height=42" />
+                </a>
+            </div>
+            <div id="logo_letter_a2" class="logo_letter">
+                <a href="http://wewillraakyou.com/" title="RAAK | Digital &amp; Social Media Agency London" rel="home">
+                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/eb9895b79c7e1cd240cb3960e16ad30b.jpg&width=35&height=42" />
+                </a>
+            </div>
+            <div id="logo_letter_a3" class="logo_letter">
+                <a href="http://wewillraakyou.com/" title="RAAK | Digital &amp; Social Media Agency London" rel="home">
+                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/3fe9de8c3f0b3d7286c6f7c97afd46ba.jpg&width=35&height=42" />
+                </a>
+            </div>
+            <div id="logo_letter_k4" class="logo_letter">
+                <a href="http://wewillraakyou.com/" title="RAAK | Digital &amp; Social Media Agency London" rel="home">
+                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/ba0c3f6be22d07916abf6ca1f7d37d8a.jpg&width=35&height=42" />
+                </a>
+            </div>
+        </div>
+        <div id="blog-description">Putting you in touch with your crowds</div>
+        <div id="access">
+            <div class="skip-link"><a href="#content" title="Skip to content">Skip to content</a></div>
+            <div id="menu"><ul><li class="first-page-item"><div class="first_topmenu_top"></div><div class="first_topmenu_body"></div></li><li class="page_item page-item-4 current_page_item"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/" title="Home">Home</a></div></li><li class="page_item page-item-1668"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/about/" title="About" rel="nofollow">About</a></div></li><li class="page_item page-item-2720"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/logo-project/" title="Logo Project">Logo Project</a></div></li><li class="page_item page-item-5169"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/our-products/" title="Our Products">Our Products</a></div></li><li class="page_item page-item-36"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/our-work/" title="Our work" rel="nofollow">Our work</a></div></li><li class="page_item page-item-211"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/blog/" title="Blog">Blog</a></div></li><li class="page_item page-item-341"><div class="topmenu_top"></div><div class="topmenu_body"><a href="http://wewillraakyou.com/contact/" title="Contact" rel="nofollow">Contact</a></div></li></ul></div>
+        </div><!-- #access -->
+    </header><!--  #header -->
+
+<script type="text/javascript">
+<!--
+var post_count = 10;
+var curr_page = 0;
+
+function expand (id) {
+    var elements_to_hide = new Array();
+    elements_to_hide[0] = 'social-media-ready';
+    elements_to_hide[1] = 'impact-projects';
+    elements_to_hide[2] = 'consulting';
+
+    var element_to_expand = document.getElementById (id);
+    var menu_item_to_activate = document.getElementById ("bluebox_body_nav_"+id);
+    var element_to_hide;
+
+    for (i = 0; i < 3; i++) {
+        element_to_hide = document.getElementById(elements_to_hide[i]);
+        menu_item_to_deactivate = document.getElementById("bluebox_body_nav_"+elements_to_hide[i]);
+        if (element_to_hide.style.display != 'none') {
+            element_to_hide.style.display = 'none';
+        }
+        if (menu_item_to_deactivate.className == "bluebox_body_nav_item active") {
+            menu_item_to_deactivate.className = "bluebox_body_nav_item";
+        }
+    }
+
+    element_to_expand.style.display = 'block';
+    menu_item_to_activate.className = "bluebox_body_nav_item active";
+}
+
+function previous () {
+    if ((post_count - curr_page) > 2) {
+        var element_to_hide = document.getElementById ("whitebox_primary_body_post_"+curr_page);
+        curr_page += 2;
+        var element_to_show = document.getElementById ("whitebox_primary_body_post_"+curr_page);
+        element_to_hide.style.display = 'none';
+        element_to_show.style.display = 'block';
+        if ((post_count - curr_page) > 2) {
+            document.getElementById('whitebox_primary_body_footer_prev_text').className = "whitebox_primary_body_footer_item active";
+        } else {
+            document.getElementById('whitebox_primary_body_footer_prev_text').className = "whitebox_primary_body_footer_item";
+        }
+        if (curr_page > 1) {
+            document.getElementById('whitebox_primary_body_footer_next_text').className = "whitebox_primary_body_footer_item active";
+        } else {
+            document.getElementById('whitebox_primary_body_footer_next_text').className = "whitebox_primary_body_footer_item";
+        }
+    }
+}
+
+function next () {
+    if (curr_page > 1) { 
+        var element_to_hide = document.getElementById ("whitebox_primary_body_post_"+curr_page);
+        curr_page -= 2;
+        var element_to_show = document.getElementById ("whitebox_primary_body_post_"+curr_page);
+        element_to_hide.style.display = 'none';
+        element_to_show.style.display = 'block';
+        if ((post_count - curr_page) > 2) {
+            document.getElementById('whitebox_primary_body_footer_prev_text').className = "whitebox_primary_body_footer_item active";
+        } else {
+            document.getElementById('whitebox_primary_body_footer_prev_text').className = "whitebox_primary_body_footer_item";
+        }
+        if (curr_page > 1) {
+            document.getElementById('whitebox_primary_body_footer_next_text').className = "whitebox_primary_body_footer_item active";
+        } else {
+            document.getElementById('whitebox_primary_body_footer_next_text').className = "whitebox_primary_body_footer_item";
+        }
+    }
+}
+
+//-->
+</script>
