32b69ddf3b0655c49ee492bf65e1b9d019b7939d
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-01-24 11:46:29 +0200

    tidying markup on logo project page

--- logo_project.php
+++ logo_project.php
@@ -11,46 +11,80 @@ Template Name:Logo Project
                 <header>
                     <h2 class="din-schrift blue_20">Logo Project</h2>
                 </header>
-<hr />Calling all designers, typographers and brand builders. We need your creative help to develop the new RAAK logo.</p>
-<h1>How it works</h1>
-<hr />
-<div id="__ss_5502775" style="width: 360px;"><object id="__sse5502775" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="360" height="300" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="src" value="http://static.slidesharecdn.com/swf/ssplayer2.swf?doc=raak-logo-project-101020070312-phpapp01&amp;stripped_title=raak-logoproject&amp;userName=raak" /><param name="name" value="__sse5502775" /><param name="allowfullscreen" value="true" /><embed id="__sse5502775" type="application/x-shockwave-flash" width="360" height="300" src="http://static.slidesharecdn.com/swf/ssplayer2.swf?doc=raak-logo-project-101020070312-phpapp01&amp;stripped_title=raak-logoproject&amp;userName=raak" name="__sse5502775" allowscriptaccess="always" allowfullscreen="true"></embed></object></div>
-<p>&nbsp;</p>
-<li><strong> Illustrator template</strong> <a href="http://wewillraakyou.com/RAAK-letter-Illustrator-template.zip">here</a>.</li>
-<li><strong>Photoshop template</strong> <a href="http://wewillraakyou.com/raak-letter-PS-template.zip">here</a>.</li>
-<h3 class="din-schrift blue_20">About the project</h3>
-<hr />
-This is not in the typical crowdsourcing project; oh no, that&#8217;s soooooooooo 2009. In this project you don&#8217;t design the whole logo. And the creation process won&#8217;t have a cut-off date. We really want to make this a co-creation experiment. </p>
-<p>We need our brand to reflect what we&#8217;re about: <strong>open, social, digital, participatory, creative and constantly changing</strong>. This unique branding exercise is about process as well as result. </p>
-<p>So we would love you to design 1 gorgeous, beautiful letter: an R, an A or a K. In any style you want; just show off your skills.</p>
-<p>When you submit your creation, it will become part of our animated logo loop. The R&#8217;s, A&#8217;s and K&#8217;s will rotate in a random way to create hundreds of thousands, millions of logos. Each with a unique, unexpected, beautiful, clashing combination of letters.</p>
-<p>We call it PDLB: Process-Driven Liquid Branding.<br />
-Or JPF: Just Plain Fun. </p>
-<p>Each uploaded letter (considering it ticks the technical boxes mentioned below) will get a spot on our <a href="http://wewillraakyou.com/logo-project/logo-archive">letter archive page</a>, where you can <em>include a link to your portfolio</em>. And you&#8217;re welcome, no encouraged even, to use your letter elsewhere.</p>
-<div id="right_float_link"><a href="http://www.wewillraakyou.com/2009/07/the-story-behind-our-crowd-sourced-raak-logo/">Read about our previous logo experiment ►</a></div>
-<hr />
-<h3 class="din-schrift blue_20">Technical notes:</h3>
-<ul>
-<blockquote>
-<li>Your file needs to be 700 px wide x 840 px high, 72dpi. The version that will appear in the logo is only 35&#215;42 px, but this way we can use it for print, projections,&#8230;</li>
-<li>You can download an<strong> Illustrator template</strong> <a href="http://wewillraakyou.com/RAAK-letter-Illustrator-template.zip">here</a>. And a <strong>Photoshop template</strong> <a href="http://wewillraakyou.com/raak-letter-PS-template.zip">here</a>.</li>
-<li>Your final file format needs to be JPG or PNG.</li>
-<li>Your letter needs to fully cover the 40&#215;40 pixel squares in the bottom corners of the template. That way it will link to the other letters.</li>
-<li>And it needs to touch the top line of the canvas/artboard.</li>
-<li>Only use the colour blue as in the template (RGB value is R0 G174 B239), except for any automatic anti-aliasing. And avoid tints please. Any shading should be made up of a dot pattern or hatching with stroked lines.</li>
-<li>Suggested minimum line width is 20 pixels &#8211; remember, the version on the homepage logo will be 20 times smaller.</li>
-<li>If you have any questions, email us at <a href="mailto: anRanAoraK@wewillraakyou.com">anRanAoraK@wewillraakyou.com</a>.</li>
-</blockquote>
-</ul>
-<hr/>
-<h3 class="din-schrift blue_20">The crowd, creativity &#038; brand debate</h3>
-<p><strong>Wybe Magermans</strong> of <a href="http://creatingdifference.com/blog/">WMH</a> on <a href="http://www.wewillraakyou.com/2010/10/gap-and-the-trouble-with-logos/">The Gap&#8217;s logo fiasco</a>.</p>
-<blockquote><p>Gap’s marketing department should know that their brand is not just their logo. Just like an ad campaign is not a brand. And if you think it is, then you will end up with the likes as FCUK. Any agency, marketing consultant or brand blogger will tell you that a brand in 21st century is much more expansive.</p></blockquote>
-<p><strong>Stuart Lambert</strong> of <a href="http://www.fishburn-hedges.co.uk/">Fishburn Hedges</a> on <a href="http://www.fishburn-hedges.co.uk/news/articles/towards-meaningful-creativity">Meaningful Creativity</a></p>
-<blockquote><p>Opening up the abstruse process of ‘creativity’ to the masses makes the act of creating something for a brand more accessible.  That’s a good thing.  Creativity is one of those attributes that people not only aspire to have and demonstrate but, particularly in a creative profession, measure themselves by. </p></blockquote>
-<hr/>
-<h3 class="din-schrift blue_20">For inspiration <img src='http://wewillraakyou.com/wp-includes/images/smilies/icon_wink.gif' alt=';)' class='wp-smiley' />  </h3>
-<p><object width="360" height="227"><param name="movie" value="http://www.youtube.com/v/jEojV_gq9fQ?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/jEojV_gq9fQ?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="360" height="227"></embed></object></p>
+                <hr />
+                <p>
+Calling all designers, typographers and brand builders. We need your creative help to develop the new RAAK logo.
+                </p>
+                <h2 class="din-schrift blue_20">How it works</h2>
+                <hr />
+                <div id="__ss_5502775" style="width: 360px;"><object id="__sse5502775" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="360" height="300" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"><param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" /><param name="src" value="http://static.slidesharecdn.com/swf/ssplayer2.swf?doc=raak-logo-project-101020070312-phpapp01&amp;stripped_title=raak-logoproject&amp;userName=raak" /><param name="name" value="__sse5502775" /><param name="allowfullscreen" value="true" /><embed id="__sse5502775" type="application/x-shockwave-flash" width="360" height="300" src="http://static.slidesharecdn.com/swf/ssplayer2.swf?doc=raak-logo-project-101020070312-phpapp01&amp;stripped_title=raak-logoproject&amp;userName=raak" name="__sse5502775" allowscriptaccess="always" allowfullscreen="true"></embed></object>
+                </div>
+                <p>&nbsp;</p>
+                <ul>
+                    <li><strong> Illustrator template</strong> <a href="http://wewillraakyou.com/RAAK-letter-Illustrator-template.zip">here</a>.</li>
+                    <li><strong>Photoshop template</strong> <a href="http://wewillraakyou.com/raak-letter-PS-template.zip">here</a>.</li>
+                </ul>
+                <h3 class="din-schrift blue_20">About the project</h3>
+                <hr />
+                <p>
+This is not in the typical crowdsourcing project; oh no, that&#8217;s soooooooooo 2009. In this project you don&#8217;t design the whole logo. And the creation process won&#8217;t have a cut-off date. We really want to make this a co-creation experiment.
+                </p>
+                <p>
+We need our brand to reflect what we&#8217;re about: <strong>open, social, digital, participatory, creative and constantly changing</strong>. This unique branding exercise is about process as well as result.
+                </p>
+                <p>
+So we would love you to design 1 gorgeous, beautiful letter: an R, an A or a K. In any style you want; just show off your skills.
+                </p>
+                <p>
+When you submit your creation, it will become part of our animated logo loop. The R&#8217;s, A&#8217;s and K&#8217;s will rotate in a random way to create hundreds of thousands, millions of logos. Each with a unique, unexpected, beautiful, clashing combination of letters.
+                </p>
+                <p>
+We call it PDLB: Process-Driven Liquid Branding.<br />
+Or JPF: Just Plain Fun.
+                </p>
+                <p>
+Each uploaded letter (considering it ticks the technical boxes mentioned below) will get a spot on our <a href="http://wewillraakyou.com/logo-project/logo-archive">letter archive page</a>, where you can <em>include a link to your portfolio</em>. And you&#8217;re welcome, no encouraged even, to use your letter elsewhere.
+                </p>
+                <div id="right_float_link">
+                    <a href="http://www.wewillraakyou.com/2009/07/the-story-behind-our-crowd-sourced-raak-logo/">Read about our previous logo experiment ►</a>
+                </div>
+                <hr />
+                <h3 class="din-schrift blue_20">Technical notes:</h3>
+                <ul>
+                    <blockquote>
+                    <li>Your file needs to be 700 px wide x 840 px high, 72dpi. The version that will appear in the logo is only 35&#215;42 px, but this way we can use it for print, projections,&#8230;</li>
+                    <li>You can download an<strong> Illustrator template</strong> <a href="http://wewillraakyou.com/RAAK-letter-Illustrator-template.zip">here</a>. And a <strong>Photoshop template</strong> <a href="http://wewillraakyou.com/raak-letter-PS-template.zip">here</a>.</li>
+                    <li>Your final file format needs to be JPG or PNG.</li>
+                    <li>Your letter needs to fully cover the 40&#215;40 pixel squares in the bottom corners of the template. That way it will link to the other letters.</li>
+                    <li>And it needs to touch the top line of the canvas/artboard.</li>
+                    <li>Only use the colour blue as in the template (RGB value is R0 G174 B239), except for any automatic anti-aliasing. And avoid tints please. Any shading should be made up of a dot pattern or hatching with stroked lines.</li>
+                    <li>Suggested minimum line width is 20 pixels &#8211; remember, the version on the homepage logo will be 20 times smaller.</li>
+                    <li>If you have any questions, email us at <a href="mailto: anRanAoraK@wewillraakyou.com">anRanAoraK@wewillraakyou.com</a>.</li>
+                    </blockquote>
+                </ul>
+                <hr/>
+                <h3 class="din-schrift blue_20">The crowd, creativity &#038; brand debate</h3>
+                <p>
+<strong>Wybe Magermans</strong> of <a href="http://creatingdifference.com/blog/">WMH</a> on <a href="http://www.wewillraakyou.com/2010/10/gap-and-the-trouble-with-logos/">The Gap&#8217;s logo fiasco</a>.
+                </p>
+                <blockquote>
+                    <p>
+Gap’s marketing department should know that their brand is not just their logo. Just like an ad campaign is not a brand. And if you think it is, then you will end up with the likes as FCUK. Any agency, marketing consultant or brand blogger will tell you that a brand in 21st century is much more expansive.
+                    </p>
+                </blockquote>
+                <p>
+<strong>Stuart Lambert</strong> of <a href="http://www.fishburn-hedges.co.uk/">Fishburn Hedges</a> on <a href="http://www.fishburn-hedges.co.uk/news/articles/towards-meaningful-creativity">Meaningful Creativity</a>
+                </p>
+                <blockquote>
+                    <p>
+Opening up the abstruse process of ‘creativity’ to the masses makes the act of creating something for a brand more accessible.  That’s a good thing.  Creativity is one of those attributes that people not only aspire to have and demonstrate but, particularly in a creative profession, measure themselves by. 
+                    </p>
+                </blockquote>
+                <hr/>
+                <h3 class="din-schrift blue_20">For inspiration <img src='http://wewillraakyou.com/wp-includes/images/smilies/icon_wink.gif' alt=';)' class='wp-smiley' />  </h3>
+                <p>
+                    <object width="360" height="227"><param name="movie" value="http://www.youtube.com/v/jEojV_gq9fQ?fs=1&amp;hl=en_US"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/jEojV_gq9fQ?fs=1&amp;hl=en_US" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="360" height="227"></embed></object>
+                </p>
             </section><!-- #whitebox_primary -->
             <section class="bluebox bluebox_primary box rounded-corners">
                 <div id="bluebox_body">
