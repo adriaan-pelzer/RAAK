b8772bc0f82a4603e0f9355b0e51f19be989f4b8
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-01-25 12:23:14 +0200

    styling whitebox secondary number 2 on logo project

--- logo_project.php
+++ logo_project.php
@@ -337,66 +337,66 @@ Opening up the abstruse process of ‘creativity’ to the masses makes the act 
                 </div><!-- multiple_tabs -->
                 <div class="whitebox_secondary whitebox box rounded-corners">
                     <form method="post" enctype="multipart/form-data" action="">
-                        <div id="whitebox_secondary_body_upload">
+                        <div id="whitebox_secondary_upload">
                             <p>Choose the letter you've designed</p>
-                            <div id="whitebox_secondary_body_upload_letters">
+                            <div id="whitebox_secondary_upload_letters">
                                 <input id="upload_letter" type="hidden" name="upload_letter" value="R" />
-                                <span id="letter_R"><a><img alt="R" src="http://wewillraakyou.com/wp-content/themes/RAAK/images/ar.jpg" /></a></span>
-                                <span id="letter_A"><a><img alt="A" src="http://wewillraakyou.com/wp-content/themes/RAAK/images/ay1.jpg" /></a></span>
-                                <span id="letter_K"><a><img alt="K" src="http://wewillraakyou.com/wp-content/themes/RAAK/images/kay.jpg" /></a></span>
+                                <span class="letter" id="letter_R"><a><img alt="R" src="http://wewillraakyou.com/wp-content/themes/RAAK/images/ar.jpg" /></a></span>
+                                <span class="letter" id="letter_A"><a><img alt="A" src="http://wewillraakyou.com/wp-content/themes/RAAK/images/ay1.jpg" /></a></span>
+                                <span class="letter" id="letter_K"><a><img alt="K" src="http://wewillraakyou.com/wp-content/themes/RAAK/images/kay.jpg" /></a></span>
                             </div>
-                            <div id="whitebox_secondary_body_upload_next">
-                                <a>Next &#9658;</a>
+                            <div id="whitebox_secondary_upload_next">
+                                <a class="smaller_arial_caps" >Next &#9658;</a>
                             </div>
                         </div><!-- #whitebox_secondary_body_upload -->
-                        <div id="whitebox_secondary_body_submit" style="display: none;">
-                            <div id="whitebox_secondary_body_submit_name">
+                        <div id="whitebox_secondary_submit" style="display: none;">
+                            <div id="whitebox_secondary_submit_name">
                                 <label for="upload_name">Your Name</label>
                                 <input id="upload_name" name="upload_name" type="text" maxlength="40" />
                             </div>
-                            <div id="whitebox_secondary_body_submit_email">
+                            <div id="whitebox_secondary_submit_email">
                                 <label for="upload_email">Email</label>
                                 <input id="upload_email" name="upload_email" type="text" maxlength="255" />
                             </div>
-                            <div id="whitebox_secondary_body_submit_url">
+                            <div id="whitebox_secondary_submit_url">
                                 <label for="upload_url">URL</label>
                                 <input id="upload_url" name="upload_url" type="text" maxlength="255" />
                             </div>
-                            <div id="whitebox_secondary_body_submit_file">
+                            <div id="whitebox_secondary_submit_file">
                                 <input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
                                 <label for="upload_file">Browse for file</label>
                                 <span id="file_replace"><input id="upload_file" name="upload_file" type="file" /><p id="dummy_file_text"></p></span>
                             </div>
-                            <div id="whitebox_secondary_body_submit_agree">
+                            <div id="whitebox_secondary_submit_agree">
                             <label for="upload_agree">I agree to the <a href="http://wewillraakyou.com/logo-project/terms-and-conditions/">terms & conditions</a></label>
                                 <input id="upload_agree" name="upload_agree" type="checkbox" />
                                 <input name="upload_submit" type="submit" value="Submit &#9658;" />
                             </div>
-                            <div class="whitebox_secondary_back" id="whitebox_secondary_body_submit_back">
+                            <div class="whitebox_secondary_back" id="whitebox_secondary_submit_back">
                                 <a>&#9668; Go back</a>
                             </div>
-                        </div><!-- #whitebox_secondary_body_submit -->
-                        <div id="whitebox_secondary_body_preview" style="display: none;">
-                            <div id="whitebox_secondary_body_preview_letters">
+                        </div><!-- #whitebox_secondary_submit -->
+                        <div id="whitebox_secondary_preview" style="display: none;">
+                            <div id="whitebox_secondary_preview_letters">
                                 <span id="preview_letter_R"><img alt="R" src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/a5605d2e128aaa3779904d517d211942.png&width=70&height=82" /></span>
                                 <span id="preview_letter_A1"><img alt="A" src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/45d36d1d36846e6a7210254b7c10b1e0.png&width=70&height=82" /></span>
                                 <span id="preview_letter_A2"><img alt="A" src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/27027cdc0a51f42ce66c576cb4916fa1.png&width=70&height=82" /></span>
                                 <span id="preview_letter_K"><img alt="K" src="http://wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/94f1cb321eb37b70e6f4a789514aee55.jpg&width=70&height=82" /></span>
                             </div>
-                            <div id="whitebox_secondary_body_preview_submit">
+                            <div id="whitebox_secondary_preview_submit">
                                 <input name="preview_submit" type="submit" value="HAPPY? Then SUBMIT your letter &#9658;" />
                             </div>
-                            <div class="whitebox_secondary_back" id="whitebox_secondary_body_preview_back">
+                            <div class="whitebox_secondary_back" id="whitebox_secondary_preview_back">
                                 <a>&#9668; Go back</a>
                             </div>
-                        </div><!-- #whitebox_secondary_body_preview -->
-                        <div id="whitebox_secondary_body_finish" style="display: none;">
+                        </div><!-- #whitebox_secondary_preview -->
+                        <div id="whitebox_secondary_finish" style="display: none;">
                             <h3>THANKS for taking part!</h3>
                             <p>Your letter is now part of the loop.</p>
                             <button id="again">Upload another letter</button>
-                        </div><!-- #whitebox_secondary_body_finish -->
+                        </div><!-- #whitebox_secondary_finish -->
                     </form>
-                </div><!-- #whitebox_secondary_body -->
+                </div><!-- whitebox_secondary -->
             </section><!-- whitebox_secondary -->
 		</div><!-- #content -->
 <?php get_sidebar() ?>
