f3aa0064a79b1f5d4da3343248a295ba30033b5a
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-01-26 14:29:59 +0200

    creating single product template

---
+++ single_product.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Template Name:Single Product 
+ */
+?>
+<?php get_header() ?>
+
+	<div id="container">
+        <div id="container_top"></div>
+		<div id="content">
+            <div id="whitebox_big">
+                <div id="whitebox_big_top"></div>
+                <div id="whitebox_big_body">
+                    <div id="whitebox_big_body_title">Our Products</div>
+                    <div class="whitebox_big_body_nav">
+                    </div><!-- #whitebox_big_body_nav -->
+                    <hr />
+                </div><!-- #whitebox_big_body -->
+                <div id="whitebox_big_bottom"></div>
+            </div><!-- #whitebox_big -->
+            <div id="whitebox_primary">
+                <div id="whitebox_primary_top_outward"><!-- for the sake of the outward-facing corner on the right --></div>
+                <div id="whitebox_primary_body">
+                    <div id="whitebox_primary_body_project" class="whitebox_primary_body_section">
+                        <span class="whitebox_primary_body_section_title">Product:</span>
+                        <span class="whitebox_primary_body_section_content"><a target="_blank" href="http://m.tweetrewinder.com">Rewinder</a></span>
+                    </div><!-- .whitebox_primary_body_section -->
+                    <hr class="solid">
+                    <div id="whitebox_primary_body_solution" class="whitebox_primary_body_section">
+                        <span class="whitebox_primary_body_section_title">Descript:</span>
+                        <span class="whitebox_primary_body_section_content"><p>Rewinder is our mobile solution to make Twitter useful with Time-shifted TV</p>
+<p>Connect your Twitter account, and we'll allow you to view your past Twitter timeline, as well as recorded hashtags, as if they were happening right now!</p>
+<p>If you're just quickly catching up with an event, you can play them back at faster than realtime too.</p>
+<p>This is as close to Time Travel you'll ever get, without altering the future!</p></span>
+                    </div><!-- .whitebox_primary_body_section -->
+                </div><!-- #whitebox_primary_body -->
+                <div id="whitebox_primary_bottom"></div>
+            </div><!-- #whitebox_primary -->
+            <div id="bluebox">
+                <div id="bluebox_top"></div>
+                <div id="bluebox_body">
+                    <div id="bluebox_body_bigpic"></div>
+                    <hr class="solid" />
+                    <div id="bluebox_body_thumbpic">
+                        
+<div class="ngg-galleryoverview" id="ngg-gallery-25-5177">
+
+
+	<!-- Piclense link -->
+	<div class="piclenselink">
+		<a class="piclenselink" href="javascript:PicLensLite.start({feedUrl:'http://wewillraakyou.com/wp-content/plugins/nextgen-gallery/xml/media-rss.php?gid=25&amp;mode=gallery'});">
+			[View with PicLens]		</a>
+	</div>
+	
+	<!-- Thumbnails -->
+		
+	<div id="ngg-image-167" class="ngg-gallery-thumbnail-box"  >
+		<div class="ngg-gallery-thumbnail" >
+			<a href="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder00.png" title=" " class="shutterset_set_25" >
+								<img title="rewinder00" alt="rewinder00" src="http://wewillraakyou.com/wp-content/gallery/rewinder/thumbs/thumbs_rewinder00.png" width="100" height="75" />
+							</a>
+		</div>
+	</div>
+	
+		
+ 		
+	<div id="ngg-image-168" class="ngg-gallery-thumbnail-box"  >
+		<div class="ngg-gallery-thumbnail" >
+			<a href="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder01.png" title=" " class="shutterset_set_25" >
+								<img title="rewinder01" alt="rewinder01" src="http://wewillraakyou.com/wp-content/gallery/rewinder/thumbs/thumbs_rewinder01.png" width="100" height="75" />
+							</a>
+		</div>
+	</div>
+	
+		
+ 		
+	<div id="ngg-image-169" class="ngg-gallery-thumbnail-box"  >
+		<div class="ngg-gallery-thumbnail" >
+			<a href="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder02.png" title=" " class="shutterset_set_25" >
+								<img title="rewinder02" alt="rewinder02" src="http://wewillraakyou.com/wp-content/gallery/rewinder/thumbs/thumbs_rewinder02.png" width="100" height="75" />
+							</a>
+		</div>
+	</div>
+	
+		
+ 	 	
+	<!-- Pagination -->
+ 	<div class='ngg-navigation'><span>1</span><a class="page-numbers" href="/2011/11/rewinder-timeshift-your-tweets/?nggpage=2">2</a><a class="page-numbers" href="/2011/11/rewinder-timeshift-your-tweets/?nggpage=3">3</a><a class="next" id="ngg-next-2" href="/2011/11/rewinder-timeshift-your-tweets/?nggpage=2">&#9658;</a></div> 	
+</div>
+
+
+                    </div>
+                </div><!-- #bluebox_body -->
+                <div id="bluebox_bottom"></div>
+            </div><!-- #bluebox -->
+		</div><!-- #content -->
+<?php get_sidebar() ?>
+<?php get_footer() ?>
