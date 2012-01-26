ce7b62bd01091dac77f4560c7ea7097826549a75
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-01-26 18:04:06 +0200

    creating single work template

---
+++ single_work.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Template Name:Single Work 
+ */
+?>
+<?php get_header() ?>
+
+	<div id="container">
+        <div id="container_top"></div>
+		<div id="content">
+            <div id="whitebox_big">
+                <div id="whitebox_big_top"></div>
+                <div id="whitebox_big_body">
+                    <div id="whitebox_big_body_title">Our Work</div>
+                    <div class="whitebox_big_body_nav">
+                        <span id="whitebox_big_body_nav_all-projects" class="whitebox_big_body_nav_item"><a href="http://wewillraakyou.com/our-work/?category=all-projects">All Projects</a></span>
+                        <span class="seperator">|</span>
+                        <span id="whitebox_big_body_nav_social-media-ready" class="whitebox_big_body_nav_item"><a href="http://wewillraakyou.com/our-work/?category=social-media-ready">social media ready</a></span>
+                        <span class="seperator">|</span>
+                        <span id="whitebox_big_body_nav_impact-projects" class="whitebox_big_body_nav_item active"><a href="http://wewillraakyou.com/our-work/?category=impact-projects">impact projects</a></span>
+                        <span class="seperator">|</span>
+                        <span id="whitebox_big_body_nav_consulting" class="whitebox_big_body_nav_item"><a href="http://wewillraakyou.com/our-work/?category=consulting">Consulting</a></span>
+                    </div><!-- #whitebox_big_body_nav -->
+                    <hr />
+                </div><!-- #whitebox_big_body -->
+                <div id="whitebox_big_bottom"></div>
+            </div><!-- #whitebox_big -->
+            <div id="whitebox_primary">
+                <div id="whitebox_primary_top_outward"><!-- for the sake of the outward-facing corner on the right --></div>
+                <div id="whitebox_primary_body">
+                    <div id="whitebox_primary_body_client" class="whitebox_primary_body_section">
+                        <span class="whitebox_primary_body_section_title">Client:</span>
+                        <span class="whitebox_primary_body_section_content">Ted Baker / Guided Collective</span>
+                    </div><!-- .whitebox_primary_body_section -->
+                    <hr class="solid">
+                    <div id="whitebox_primary_body_project" class="whitebox_primary_body_section">
+                        <span class="whitebox_primary_body_section_title">Project:</span>
+                        <span class="whitebox_primary_body_section_content">Ted Baker's Rutting Season</span>
+                    </div><!-- .whitebox_primary_body_section -->
+                    <hr class="solid">
+                    <div id="whitebox_primary_body_brief" class="whitebox_primary_body_section">
+                        <span class="whitebox_primary_body_section_title">Brief:</span>
+                        <span class="whitebox_primary_body_section_content">Create more awareness for Ted Baker and Ted Baker's Facebook page.</span>
+                    </div><!-- .whitebox_primary_body_section -->
+                    <hr class="solid">
+                    <div id="whitebox_primary_body_solution" class="whitebox_primary_body_section">
+                        <span class="whitebox_primary_body_section_title">Solution:</span>
+                        <span class="whitebox_primary_body_section_content"><p>So Ted Baker wanted to boost their Facebook presence. But building a Facebook fan page is hard work.</p>
+<p>Unless you do something extraordinary. We combined Facebook with the power of the photosharing app <strong>Instagram</strong>.</p> 
+<p>So we built an app that allowed Ted Baker to send gorgeous pictures of their customers straight to their Facebook page.</p>
+<p>The result was a campaign where the most gorgeous does and the most rampant stags were photographed in store. And those with the most Likes on Facebook won a lovely prize.</p>
+<p>The campaign not only drove hundreds of Likes to each bit of Ted Baker content, it raised awareness over all.</p></span>
+                    </div><!-- .whitebox_primary_body_section -->
+                </div><!-- #whitebox_primary_body -->
+                <div id="whitebox_primary_bottom"></div>
+            </div><!-- #whitebox_primary -->
+            <div id="bluebox">
+                <div id="bluebox_top"></div>
+                <div id="bluebox_body">
+                    <div id="bluebox_body_bigpic"></div>
+                    <hr class="solid" />
+                    <div id="bluebox_body_thumbpic">
+                        
+<div class="ngg-galleryoverview" id="ngg-gallery-23-5118">
+
+
+	<!-- Piclense link -->
+	<div class="piclenselink">
+		<a class="piclenselink" href="javascript:PicLensLite.start({feedUrl:'http://wewillraakyou.com/wp-content/plugins/nextgen-gallery/xml/media-rss.php?gid=23&amp;mode=gallery'});">
+			[View with PicLens]		</a>
+	</div>
+	
+	<!-- Thumbnails -->
+		
+	<div id="ngg-image-154" class="ngg-gallery-thumbnail-box"  >
+		<div class="ngg-gallery-thumbnail" >
+			<a href="http://wewillraakyou.com/wp-content/gallery/ruttingseason/tedbaker2.jpg" title=" " class="shutterset_set_23" >
+								<img title="tedbaker2" alt="tedbaker2" src="http://wewillraakyou.com/wp-content/gallery/ruttingseason/thumbs/thumbs_tedbaker2.jpg" width="100" height="75" />
+							</a>
+		</div>
+	</div>
+	
+		
+ 		
+	<div id="ngg-image-161" class="ngg-gallery-thumbnail-box"  >
+		<div class="ngg-gallery-thumbnail" >
+			<a href="http://wewillraakyou.com/wp-content/gallery/ruttingseason/tedbaker6copy.jpg" title=" " class="shutterset_set_23" >
+								<img title="tedbaker6copy" alt="tedbaker6copy" src="http://wewillraakyou.com/wp-content/gallery/ruttingseason/thumbs/thumbs_tedbaker6copy.jpg" width="100" height="75" />
+							</a>
+		</div>
+	</div>
+	
+		
+ 		
+	<div id="ngg-image-156" class="ngg-gallery-thumbnail-box"  >
+		<div class="ngg-gallery-thumbnail" >
+			<a href="http://wewillraakyou.com/wp-content/gallery/ruttingseason/tedbaker4.jpg" title=" " class="shutterset_set_23" >
+								<img title="tedbaker4" alt="tedbaker4" src="http://wewillraakyou.com/wp-content/gallery/ruttingseason/thumbs/thumbs_tedbaker4.jpg" width="100" height="75" />
+							</a>
+		</div>
+	</div>
+	
+		
+ 	 	
+	<!-- Pagination -->
+ 	<div class='ngg-clear'></div>
+ 	
+</div>
+
+
+                    </div>
+                </div><!-- #bluebox_body -->
+                <div id="bluebox_bottom"></div>
+            </div><!-- #bluebox -->
+		</div><!-- #content -->
+<?php get_sidebar() ?>
+<?php get_footer() ?>
