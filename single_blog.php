3a0a2ffee7e97c82f17469453793b4dd8d14ff9e
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-01-30 15:16:28 +0200

    creating single blog template

---
+++ single_blog.php
@@ -0,0 +1,181 @@
+<?php
+/*
+Template Name:Single Blog 
+ */
+?>
+<?php get_header() ?>
+
+	<div id="container">
+        <div id="container_top"></div>
+		<div id="content">
+
+            <div id="whitebox_big">
+                <div id="whitebox_big_top"></div>
+                <div id="whitebox_big_body">
+                    <span id="whitebox_big_body_title"><h1>Our Blog</h1></span>
+                    <span id="whitebox_big_body_search"><a onclick="javascript: google_search();">Search</a><input type="text" id="searchtext" /></span>
+                </div><!-- #whitebox_big_body -->
+                <div id="whitebox_big_bottom"></div>
+            </div><!-- #whitebox_big -->
+
+            <div id="whitebox_primary">
+                <div id="whitebox_primary_top">
+                </div><!-- #whitebox_primary_top -->
+                <div id="whitebox_primary_body">
+                    <div class="whitebox_primary_body_post">
+                        <div id="whitebox_primary_body_title"><h1>No wonder the MPAA is freaking out: Youtube serves 4 billion movies per day</h1></div>
+                        <hr />
+                        <div class="whitebox_primary_body_attr">
+                            <div id="whitebox_primary_body_attr_author" class="whitebox_primary_body_attr_item">Posted by <a rel="author" href="http://wewillraakyou.com/about/the-founders/adriaan/">Adriaan Pelzer</a></div>
+                            <div id="whitebox_primary_body_attr_date" class="whitebox_primary_body_attr_item">27 January 2012</div>
+                            <div id="whitebox_primary_body_attr_time" class="whitebox_primary_body_attr_item">12:38</div>
+                            <div id="whitebox_primary_body_attr_comment" class="whitebox_primary_body_attr_item"><span id="whitebox_primary_body_attr_commenticon"><img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/whitebox_primary_body_attr_comment_icon.png" /></span><span id="whitebox_primary_body_attr_comments">0 comments</span></div>
+                        </div><!-- .whitebox_primary_body_attr -->
+                        <div class="whitebox_primary_body_share">
+                            <a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal" data-via="adriaan_pelzer" data-related="RAAKonteurs" data-text="No wonder the MPAA is freaking out: Youtube serves 4 billion movies per day &#9733; RAAK">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+<!--begin facebook like code--><div align="center" style="text-align: center;padding: 10px;" class="opengraphprotocoltools-div"><iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwewillraakyou.com%2F2012%2F01%2Fno-wonder-the-mpaa-is-trying-to-break-the-internet-youtube-serves-4-billion-movies-per-day%2F&amp;layout=button_count&amp;show_faces=true&amp;width=450&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:450px; height:80px"></iframe></div><!--end facebook like code-->                            <div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:send font=""></fb:send>
+                            <div id="plusone"><!--iframe allowtransparency="true" frameborder="0" hspace="0" id="I1_1307012621585" marginheight="0" marginwidth="0" name="I1_1307012621585" scrolling="no" src="https://plusone.google.com/u/0/_/+1/button?hl=en-US&amp;jsh=s%3Bplusone%3Agoogleapis.client%4021550740_8d71de52%2Fclient%3Bgoogleapis.proxy%4021550740_8d71de52%2Fproxy%3Bplusone%3Agoogleapis.client%3Aiframes-styles-bubble%4021550740_8d71de52%2Fbubble%3Biframes-styles-bubble!plusone%3Agoogleapis.client%4021550740_8d71de52%2Fbubble_only%3Bplusone-unsupported%4021550740_8d71de52%2Funsupported#url=http%3A%2F%2Fwewillraakyou.com%2F2012%2F01%2Fno-wonder-the-mpaa-is-trying-to-break-the-internet-youtube-serves-4-billion-movies-per-day%2F&amp;size=medium&amp;count=true&amp;id=I1_1307012621585&amp;parent=http%3A%2F%2Fwewillraakyou.com&amp;rpctoken=982298531&amp;_methods=_ready%2C_close%2C_open%2C_resizeMe" style="width: 82px; height: 20px; position: static; left: 0px; top: 0px; visibility: visible; " tabindex="-1" vspace="0" width="100%"></iframe--><g:plusone size="medium"></g:plusone></div>
+                        </div><!-- .whitebox_primary_body_share -->
+                        <hr class="solid" />
+                        <div class="whitebox_primary_body_content">
+                            <p>Youtube released a set of  extremely impressive stats this week. Not only does it serve a staggering <a href="http://www.engadget.com/2012/01/23/youtube-hit-4-billion-views-per-day-deals-with-60-hours-of-uplo/">4 billion videos per day</a>, 60 hours of new content is uploaded every minute. This is massive.</p>
+<p>Now, let&#8217;s have a look at where this leaves the MPAA:</p>
+<p><img src="http://wewillraakyou.com/wp-content/uploads/2012/01/youtube-censored-2.png" alt="Youtube vs MPAA" title="Youtube vs MPAA" width="360" height="232" class="alignnone size-full wp-image-5275" /></p>
+<p>The MPAA represents the big content resellers of the 20th century, whose business model used to be valid in a world where content was scarce. That used to be the case in almost the entire 20th century. Clay Shirky explains this quite well in a <a href="http://www.intellitics.com/blog/2012/01/18/clay-shirky-ted-talk-defend-our-freedom-to-share-or-why-sopa-is-a-bad-idea/">brilliant Ted Talk</a> on the subject.</p>
+<p>The fact that content is not scarce anymore, the MPAA feels, is the Internet&#8217;s fault, and this is why they are actively, and desperately trying to break the Internet. In <a href="http://www.launch.is/blog/we-need-to-empower-hollywood-not-kill-hollywood.html">a heartfelt post</a> Jason Calacanis explains that proponents of the Internet should not see this as a threat:</p>
+<blockquote><p>The internet is more powerful than any of us thought &#8212; and it&#8217;s getting more powerful every day. Hollywood brings a lot to the party and while it can be misguided at times, it&#8217;s not productive to say we&#8217;re going to kill it. </p>
+<p>Silicon Valley&#8217;s job is to empower Hollywood and make it appreciate what we&#8217;ve built. In the same way it makes us appreciate its products &#8212; even garbage like &#8220;Transformers&#8221; 1, 2 and 3 which, sadly, most of us have wasted money on.</p></blockquote>
+<p>If that sentiment will be reciprocal remains to be seen, however.</p>
+<p>The only product the MPAA have is a channel. A channel to market and distribute content on. And they are still using that same old channel. They are still showing you advertisements for their next movies before the movie you&#8217;re trying to watch.</p>
+<p>Now, this very channel of theirs, has been replaced. By the Internet.</p>
+<p><em>This</em> is why the MPAA is spending millions of dollars to actively break the Internet.</p>
+                        </div><!-- .whitebox_primary_body_content -->
+                        <div class="whitebox_primary_body_flwbtn">
+                            <p><span><em>Posted by <a rel="author" href="http://wewillraakyou.com/about/the-founders/adriaan/">Adriaan Pelzer</a></em></span><span><a href="http://www.twitter.com/adriaan_pelzer" class="twitter-follow-button" data-show-count="true">Follow @adriaan_pelzer</a></span></p>
+                            <script src="http://platform.twitter.com/widgets.js" type="text/javascript"></script>
+                        </div>
+                    </div><!-- .whitebox_primary_body_post -->
+                    <div id="whitebox_primary_body_comments">
+			<div id="comments">
+
+				<div id="respond">
+					<h3>Leave a Comment</h3>
+
+					<div class="formcontainer">	
+						<form name="commentform" id="commentform" action="http://wewillraakyou.com/wp-comments-post.php" method="post">
+
+
+							<p id="comment-notes">Your email is <em>never</em> shared. Required fields are marked <span class="required">*</span></p>
+
+							<div class="form-label"><label for="author">Name</label> <span class="required">*</span></div>
+							<div class="form-input"><input id="author" name="author" class="text required" type="text" value="" size="30" maxlength="50" tabindex="3" /></div>
+
+							<div class="form-label"><label for="email">Email</label> <span class="required">*</span></div>
+							<div class="form-input"><input id="email" name="email" class="text required" type="text" value="" size="30" maxlength="50" tabindex="4" /></div>
+
+							<div class="form-label"><label for="url">Website</label></div>
+							<div class="form-input"><input id="url" name="url" class="text" type="text" value="" size="30" maxlength="50" tabindex="5" /></div>
+
+
+							<div class="form-label"><label for="comment">Comment</label></div>
+							<div class="form-textarea"><textarea id="comment" name="comment" class="text required" cols="45" rows="8" tabindex="6"></textarea></div>
+
+							<div class="form-submit"><input type="hidden" name="submit" value="Post Comment" />Post Comment<!--span id="commentform_submit"--><input id="submit" name="submit" class="button" type="submit" value="Post Comment" tabindex="7" /><!--/span--><input type="hidden" name="comment_post_ID" value="5273" /></div>
+
+							<div class="form-option"></div>
+
+						</form><!-- #commentform -->
+					</div><!-- .formcontainer -->
+
+				</div><!-- #respond -->
+
+			</div><!-- #comments -->
+                    </div><!-- #whitebox_primary_body_comments -->
+                </div><!-- #whitebox_primary_body -->
+                <div id="whitebox_primary_bottom"></div>
+            </div><!-- #whitebox_primary -->
+            <div id="bluebox">
+                <div id="bluebox_tab">
+                    <div id="bluebox_tab_top"></div>
+                    <div id="bluebox_tab_body">Related Posts</div>
+                </div><!-- #bluebox_tab -->
+                <div id="bluebox_top"></div>
+                <div id="bluebox_body">
+                    <div id="bluebox_body_top">
+                        <div id="bluebox_body_left_top">
+                            <div id="bluebox_body_left_top_image">
+                                <a href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/"><img title="ebooks" src="http://wewillraakyou.com/wp-content/uploads/2012/01/ebooks.jpeg" alt="ebooks"  width="120" height="85" /></a>
+                            </div>
+                            <div id="bluebox_body_left_top_title">
+                                <a href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/">The RAAKonteur #69 - Apple out to smash textbook publishing & SEO by Celebrity</a>
+                            </div>
+                        </div>
+                        <div id="bluebox_body_right_top">
+                            <div id="bluebox_body_right_top_image">
+                                <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/12/friend_request_from_your_unborn_child.jpg" alt="friend_request_from_your_unborn_child" title="friend_request_from_your_unborn_child"  width="120" height="85" /></a>
+                            </div>
+                            <div id="bluebox_body_right_top_title">
+                                <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">The RAAKonteur #66 - Spotify Platform, How people look at your Facebook profile, Wordpress Ads and more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr id="hrleft" class="solid" />
+                    <hr id="hrright" class="solid" />
+                    <div id="bluebox_body_bottom">
+                        <div id="bluebox_body_left_bottom">
+                            <div id="bluebox_body_left_bottom_image">
+                                <a href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/11/matt_mullenweg.jpeg" alt="matt_mullenweg" title="matt_mullenweg"  width="120" height="85" /></a>
+                            </div>
+                            <div id="bluebox_body_left_bottom_title">
+                                <a href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/">The RAAKonteur #63 - Wordpress is going Social, Google+ has a minus, and the dangerous world of Slacktivism</a>
+                            </div>
+                        </div>
+                        <div id="bluebox_body_right_bottom">
+                            <div id="bluebox_body_right_bottom_image">
+                                <a href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/08/social_media_cost.png" alt="social_media_cost" title="social_media_cost"  width="120" height="85" /></a>
+                            </div>
+                            <div id="bluebox_body_right_bottom_title">
+                                <a href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/">The RAAKonteur #52 - Real cost of Social Media, Is Newt Gingrich a cheat & deep Facebook analytics</a>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- bluebox_body -->
+                <div id="bluebox_bottom"></div>
+            </div><!-- #bluebox -->
+            <div id="whitebox_secondary">
+                <div id="whitebox_secondary_tabs">
+                    <div id="whitebox_secondary_tabs_mostviewed" class="whitebox_secondary_tab active">
+                        <div class="whitebox_secondary_tab_top"></div>
+                        <div class="whitebox_secondary_tab_body"><a onclick="javascript: expand('mostviewed');">Most Viewed</a></div>
+                    </div>
+                    <div id="whitebox_secondary_tabs_category" class="whitebox_secondary_tab">
+                        <div class="whitebox_secondary_tab_top"></div>
+                        <div class="whitebox_secondary_tab_body"><a onclick="javascript: expand('category');">Category</a></div>
+                    </div>
+                </div><!-- #whitebox_secondary_tabs -->
+                <div id="whitebox_secondary_top"></div>
+                <div id="whitebox_secondary_body">
+                    <div id="whitebox_secondary_body_mostviewed">
+                        <ul>
+                            <li><a href="http://wewillraakyou.com/2010/12/klout-is-broken/">Klout is broken</a></li>
+                            <li><a href="http://wewillraakyou.com/2011/06/google-plusone-button-howto/">How to add a Google +1 button to your website</a></li>
+                            <li><a href="http://wewillraakyou.com/2010/02/the-answer-to-die-antwoords-marketing-social-media/">The answer to Die Antwoord's marketing success</a></li>
+                            <li><a href="http://wewillraakyou.com/2010/07/facebook-extended-permissions-theyre-not-as-bad-as-you-think-they-are/">Facebook Extended Permissions – they're not as bad as you think they are</a></li>
+                            <li><a href="http://wewillraakyou.com/2011/06/twitter-steals-devnest/">Twitter eats its Babies</a></li>
+                            <li><a href="http://wewillraakyou.com/2010/10/how-do-url-shorteners-work/">How do URL shorteners work?</a></li>
+                        </ul>
+                    </div><!-- #whitebox_secondary_body_mostviewed -->
+                    <div id="whitebox_secondary_body_category" style="display: none">
+                        <ul>
+                            <li><a href="http://wewillraakyou.com/category/Inspiration/">Inspiration</a></li>
+                            <li><a href="http://wewillraakyou.com/category/Must Read/">Must Read</a></li>
+                            <li><a href="http://wewillraakyou.com/category/RAAKonteur/">RAAKonteur</a></li>
+                            <li><a href="http://wewillraakyou.com/category/Worth a look/">Worth a look</a></li>
+                        </ul>
+                    </div><!-- #whitebox_secondary_body_category -->
+                </div><!-- #whitebox_secondary_body -->
+                <div id="whitebox_secondary_bottom"></div>
+            </div><!-- #whitebox_secondary -->
+		</div><!-- #content -->
+<?php get_sidebar() ?>
+<?php get_footer() ?>
