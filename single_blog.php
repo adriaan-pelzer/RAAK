72b39c588db6be6ce02deb0add4103d140cfb3c5
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-01-31 13:54:58 +0200

    styling related posts on single blog

--- single_blog.php
+++ single_blog.php
@@ -71,40 +71,24 @@ Template Name:Single Blog
                 <div class="bluebox_primary bluebox box rounded-corners">
                     <div class="bluebox_content_top">
                         <div class="bluebox_content_quarter bluebox_content_top_left">
-                            <div class="related_posts_image">
-                                <a href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/"><img title="ebooks" src="http://wewillraakyou.com/wp-content/uploads/2012/01/ebooks.jpeg" alt="ebooks"  width="120" height="85" /></a>
-                            </div>
-                            <div class="related_posts_title">
-                                <a class="grey_text" href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/">The RAAKonteur #69 - Apple out to smash textbook publishing & SEO by Celebrity</a>
-                            </div>
+                            <a href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/"><img title="ebooks" src="http://wewillraakyou.com/wp-content/uploads/2012/01/ebooks.jpeg" alt="ebooks"  width="120" height="85" /></a>
+                            <h4><a class="grey_text" href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/">The RAAKonteur #69 - Apple out to smash textbook publishing & SEO by Celebrity</a></h4>
                         </div>
                         <div class="bluebox_content_quarter bluebox_content_top_right">
-                            <div class="related_posts_image">
-                                <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/12/friend_request_from_your_unborn_child.jpg" alt="friend_request_from_your_unborn_child" title="friend_request_from_your_unborn_child"  width="120" height="85" /></a>
-                            </div>
-                            <div class="related_posts_title">
-                                <a class="grey_text" href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">The RAAKonteur #66 - Spotify Platform, How people look at your Facebook profile, Wordpress Ads and more</a>
-                            </div>
+                            <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/12/friend_request_from_your_unborn_child.jpg" alt="friend_request_from_your_unborn_child" title="friend_request_from_your_unborn_child"  width="120" height="85" /></a>
+                            <h4><a class="grey_text" href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">The RAAKonteur #66 - Spotify Platform, How people look at your Facebook profile, Wordpress Ads and more</a></h4>
                         </div>
                     </div>
                     <hr class="solid blue_hr related_posts_hr" />
                     <hr class="solid blue_hr related_posts_hr" />
                     <div class="bluebox_content_bottom">
                         <div class="bluebox_content_quarter bluebox_content_bottom_left">
-                            <div class="related_posts_image">
-                                <a href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/11/matt_mullenweg.jpeg" alt="matt_mullenweg" title="matt_mullenweg"  width="120" height="85" /></a>
-                            </div>
-                            <div class="related_posts_title">
-                                <a class="grey_text" href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/">The RAAKonteur #63 - Wordpress is going Social, Google+ has a minus, and the dangerous world of Slacktivism</a>
-                            </div>
+                            <a href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/11/matt_mullenweg.jpeg" alt="matt_mullenweg" title="matt_mullenweg"  width="120" height="85" /></a>
+                            <h4><a class="grey_text" href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/">The RAAKonteur #63 - Wordpress is going Social, Google+ has a minus, and the dangerous world of Slacktivism</a></h4>
                         </div>
                         <div class="bluebox_content_quarter bluebox_content_bottom_right">
-                            <div class="related_posts_image">
-                                <a href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/08/social_media_cost.png" alt="social_media_cost" title="social_media_cost"  width="120" height="85" /></a>
-                            </div>
-                            <div class="related_posts_title">
-                                <a class="grey_text" href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/">The RAAKonteur #52 - Real cost of Social Media, Is Newt Gingrich a cheat & deep Facebook analytics</a>
-                            </div>
+                            <a href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/08/social_media_cost.png" alt="social_media_cost" title="social_media_cost"  width="120" height="85" /></a>
+                            <h4><a class="grey_text" href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/">The RAAKonteur #52 - Real cost of Social Media, Is Newt Gingrich a cheat & deep Facebook analytics</a></h4>
                         </div>
                     </div>
                 </div><!-- bluebox_body -->
