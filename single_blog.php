103ad51d5438d4bf91b526f14f47bf1e57cf3158
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-01-31 13:18:34 +0200

    styling comment section on single blog

--- single_blog.php
+++ single_blog.php
@@ -64,54 +64,52 @@ Template Name:Single Blog
                     </div><!-- #respond -->
                 </div><!-- whitebox_primary_comments -->
             </section><!-- whitebox_primary -->
-            <div id="bluebox">
-                <div id="bluebox_tab">
-                    <div id="bluebox_tab_top"></div>
-                    <div id="bluebox_tab_body">Related Posts</div>
-                </div><!-- #bluebox_tab -->
-                <div id="bluebox_top"></div>
-                <div id="bluebox_body">
-                    <div id="bluebox_top">
-                        <div id="bluebox_left_top">
-                            <div id="bluebox_left_top_image">
+            <section class="tab_container bluebox-primary other_posts">
+                <div class="blue_tab tab tab108 rounded-corners">
+                    <h2>Related Posts</h2>
+                </div><!-- bluebox_tab -->
+                <div class="bluebox_content">
+                    <div class="bluebox_content_top">
+                        <div class="bluebox_content_top_left">
+                            <div class="related_posts_image">
                                 <a href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/"><img title="ebooks" src="http://wewillraakyou.com/wp-content/uploads/2012/01/ebooks.jpeg" alt="ebooks"  width="120" height="85" /></a>
                             </div>
-                            <div id="bluebox_left_top_title">
+                            <div class="related_posts_title">
                                 <a href="http://wewillraakyou.com/2012/01/the-raakonteur-69-apple-out-to-smash-textbook-publishing-seo-by-celebrity/">The RAAKonteur #69 - Apple out to smash textbook publishing & SEO by Celebrity</a>
                             </div>
                         </div>
-                        <div id="bluebox_right_top">
-                            <div id="bluebox_right_top_image">
+                        <div class="bluebox_content_top_right">
+                            <div class="related_posts_image">
                                 <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/12/friend_request_from_your_unborn_child.jpg" alt="friend_request_from_your_unborn_child" title="friend_request_from_your_unborn_child"  width="120" height="85" /></a>
                             </div>
-                            <div id="bluebox_right_top_title">
+                            <div class="related_posts_title">
                                 <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">The RAAKonteur #66 - Spotify Platform, How people look at your Facebook profile, Wordpress Ads and more</a>
                             </div>
                         </div>
                     </div>
                     <hr id="hrleft" class="solid" />
                     <hr id="hrright" class="solid" />
-                    <div id="bluebox_bottom">
-                        <div id="bluebox_left_bottom">
-                            <div id="bluebox_left_bottom_image">
+                    <div class="bluebox_content_bottom">
+                        <div class="bluebox_content_bottom_left">
+                            <div class="related_posts_image">
                                 <a href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/11/matt_mullenweg.jpeg" alt="matt_mullenweg" title="matt_mullenweg"  width="120" height="85" /></a>
                             </div>
-                            <div id="bluebox_left_bottom_title">
+                            <div class="related_posts_title">
                                 <a href="http://wewillraakyou.com/2011/11/the-raakonteur-63-wordpress-is-going-social-google-has-a-minus-and-the-dangerous-world-of-slacktivism/">The RAAKonteur #63 - Wordpress is going Social, Google+ has a minus, and the dangerous world of Slacktivism</a>
                             </div>
                         </div>
-                        <div id="bluebox_right_bottom">
-                            <div id="bluebox_right_bottom_image">
+                        <div class="bluebox_content_bottom_right">
+                            <div class="related_posts_image">
                                 <a href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/08/social_media_cost.png" alt="social_media_cost" title="social_media_cost"  width="120" height="85" /></a>
                             </div>
-                            <div id="bluebox_right_bottom_title">
+                            <div class="related_posts_title">
                                 <a href="http://wewillraakyou.com/2011/08/the-raakonteur-52-real-cost-of-social-media-is-newt-gingrich-a-cheat-deep-facebook-analytics/">The RAAKonteur #52 - Real cost of Social Media, Is Newt Gingrich a cheat & deep Facebook analytics</a>
                             </div>
                         </div>
                     </div>
                 </div><!-- bluebox_body -->
                 <div id="bluebox_bottom"></div>
-            </div><!-- #bluebox -->
+            </section><!-- #bluebox -->
             <div id="whitebox_secondary">
                 <div id="whitebox_secondary_tabs">
                     <div id="whitebox_secondary_tabs_mostviewed" class="whitebox_secondary_tab active">
