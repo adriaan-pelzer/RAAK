87efa0e9fb23d0bdc73b0b9d548f44c83e4018e9
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-01 16:02:12 +0200

    changing elements to be symantically correct

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Template Name:Home
 
     <div class="container rounded-corners home">
         <div class="content">
-            <section id="logox_counter" class="rounded-corners din-schrift">
+            <aside id="logox_counter" class="rounded-corners din-schrift">
                 <div id="logox_counter_arrow">
                     <span class="point_left"></span>
                     <span class="point_right"></span>
@@ -19,13 +19,13 @@ Template Name:Home
                 <a id="read_the_logo_story" href="http://wewillraakyou.com/2010/11/the-perpetually-changing-crowdsourced-raak-logo/">Read the story behind our logo</a>
                 <hr />
                 <a id="upload_a_letter" href="http://wewillraakyou.com/wp-content/themes/RAAK/logo-project/">Upload a letter</a>
-            </section><!-- #logox_counter -->
-            <section id="bluebox_home_left" class="bluebox box rounded-corners">
+            </aside><!-- logox_counter -->
+            <div id="bluebox_home_left" class="bluebox box rounded-corners">
                 <h2 class="din-schrift"><a href="http://wewillraakyou.com/our-work/">Our Work</a></h2>
                 <hr />    
                 <p>RAAK is a social &amp; digital media <em>plug-in</em>. We put you in touch with people, be it your customers, your suppliers, or employees.</p>
-            </section><!-- #bluebox_home_left -->
-            <section id="bluebox_home_right" class="bluebox bluebox_primary_no_margin box rounded-corners">
+            </div><!-- #bluebox_home_left -->
+            <div id="bluebox_home_right" class="bluebox bluebox_primary_no_margin box rounded-corners">
                     <nav id="bluebox_nav" class="box_nav">
                         <h3 class="bluebox_nav_item small_arial_caps"><a onclick="javascript: expand('social-media-ready');">social media ready</a></h3>
                         <span class="seperator">|</span>
@@ -33,7 +33,7 @@ Template Name:Home
                         <span class="seperator">|</span>
                         <h3 class="bluebox_nav_item small_arial_caps"><a class="active" onclick="javascript: expand('consulting');">Consulting</a></h3>
                     </nav>
-                    <div class="bluebox_content" id="consulting">
+                    <section class="bluebox_content" id="social-media-ready" style="display: none;">
                         <a href="http://wewillraakyou.com/2010/06/social-media-strategy-for-the-world-economic-forum/"><img alt="" src="http://wewillraakyou.com/wp-content/gallery/wef/davos-2.png"  width="315" /></a>
                         <ul>
                             <li class="bluebox_content_sub">
@@ -52,56 +52,100 @@ Template Name:Home
                                 <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
                             </li>
                         </ul>
-                    </div>
-            </section><!-- #bluebox_home_right -->
-            <section class="tab_container whitebox-primary">
+                    </section>
+                    <section class="bluebox_content" id="impact-projects" style="display: none;">
+                        <a href="http://wewillraakyou.com/2010/06/social-media-strategy-for-the-world-economic-forum/"><img alt="" src="http://wewillraakyou.com/wp-content/gallery/wef/davos-2.png"  width="315" /></a>
+                        <ul>
+                            <li class="bluebox_content_sub">
+                                <span class="label">Client:</span>
+                                <span class="title">World Economic Forum</span>
+                            </li>
+                            <li class="bluebox_content_sub">
+                                <span class="label">Project:</span>
+                                <span class="title">WELCOM</span>
+                            </li>
+                            <li class="bluebox_content_sub">
+                                <span class="label">Overview:</span>
+                                <span class="overview">A strategy & functional specification for a social network for the world's most influential people. <br /><strong>Read More >></strong></span>
+                            </li>
+                            <li class="bluebox_content_link">
+                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
+                            </li>
+                        </ul>
+                    </section>
+                    <section class="bluebox_content" id="consulting">
+                        <a href="http://wewillraakyou.com/2010/06/social-media-strategy-for-the-world-economic-forum/"><img alt="" src="http://wewillraakyou.com/wp-content/gallery/wef/davos-2.png"  width="315" /></a>
+                        <ul>
+                            <li class="bluebox_content_sub">
+                                <span class="label">Client:</span>
+                                <span class="title">World Economic Forum</span>
+                            </li>
+                            <li class="bluebox_content_sub">
+                                <span class="label">Project:</span>
+                                <span class="title">WELCOM</span>
+                            </li>
+                            <li class="bluebox_content_sub">
+                                <span class="label">Overview:</span>
+                                <span class="overview">A strategy & functional specification for a social network for the world's most influential people. <br /><strong>Read More >></strong></span>
+                            </li>
+                            <li class="bluebox_content_link">
+                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
+                            </li>
+                        </ul>
+                    </section>
+            </div><!-- #bluebox_home_right -->
+            <div class="tab_container whitebox-primary">
                 <div class="grey_tab tab tab104 rounded-corners">
                         <h2>Latest Posts</h2>
                 </div><!-- .grey_tab -->
                 <div class="whitebox whitebox_primary box rounded-corners">
                     <div id="whitebox_primary_post_0" class="whitebox_primary_post">
+                        <article>
                             <h3><a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">The RAAKonteur #66 - Spotify Platform, How people look at your Facebook profile, Wordpress Ads and more</a></h3>
-                        <hr />
-                        <div class="whitebox_primary_post_attr">
-                            <span class="whitebox_primary_post_attr_item author">Posted by <a href="http://wewillraakyou.com/about/the-founders/gerrie/">Gerrie Smits</a></span>
-                            <span class="seperator">|</span>
-                            <span class="whitebox_primary_post_attr_item date"> 5 Dec 2011</span>
-                            <span class="seperator">|</span>
-                            <!--span class="whitebox_primary_post_attr_item time">12:44</span>
-                            <span class="seperator">|</span-->
-                            <span class="whitebox_primary_post_attr_item comments"><img class="commenticon" src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/images/whitebox_primary_body_attr_comment_icon.png" />0 comments</span>
-                        </div><!-- .whitebox_primary_post_attr -->
-                        <div class="whitebox_primary_post_content">
-                            <div class="whitebox_primary_post_content_right">
-                                <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/12/friend_request_from_your_unborn_child.jpg" alt="friend_request_from_your_unborn_child" title="friend_request_from_your_unborn_child"  width="162" height="104" /></a>
-                                <a class="more_link" href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">More &#9658;</a>
-                            </div><!-- .whitebox_primary_post_content_right -->
-                            <p>
-                                Later this week the Pulitzer Prize Board will <a href="http://www.pulitzer.org/digital_entries">announce the details</a> of their new journalism prize categories, which will emphasize real-time reporting (that&#39;s live-tweeting to us tweeps). Until then, there's the weekly RAAKonteur.
-                            </p>
-                        </div><!-- .whitebox_primary_post_content -->
-                        <hr class="solid" />
-                        <h3><a href="http://wewillraakyou.com/2011/11/the-raakonteur-65-kevin-roses-new-project-the-facebook-freakyline-and-more/">The RAAKonteur #65 - Kevin Rose's new project, The Facebook Freakyline and more</a></h3>
-                        <hr />
-                        <div class="whitebox_primary_post_attr">
-                            <span class="whitebox_primary_post_attr_item author">Posted by <a href="http://wewillraakyou.com/about/the-founders/gerrie/">Gerrie Smits</a></span>
-                            <span class="seperator">|</span>
-                            <span class="whitebox_primary_post_attr_item date">28 Nov 2011</span>
-                            <span class="seperator">|</span>
-                            <!--span id="whitebox_primary_attr_post_time" class="whitebox_primary_post_attr_item">11:59</span>
-                            <span class="seperator">|</span-->
-                            <span class="whitebox_primary_post_attr_item comments"><img class="commenticon" src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/images/whitebox_primary_body_attr_comment_icon.png" />0 comments</span>
-                        </div><!-- .whitebox_primary_post_attr -->
-                        <div class="whitebox_primary_post_content">
-                            <div class="whitebox_primary_post_content_right">
-                                <a href="http://wewillraakyou.com/2011/11/the-raakonteur-65-kevin-roses-new-project-the-facebook-freakyline-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/11/Picture_39.png" alt="etsy-recommendations" title="etsy-recommendations"  width="162" height="104" /></a>
-                                <a class="more_link" href="http://wewillraakyou.com/2011/11/the-raakonteur-65-kevin-roses-new-project-the-facebook-freakyline-and-more/">More &#9658;</a>
-                            </div><!-- .whitebox_primary_post_content_right -->
-                            <p>
-                                This week we are trying out a frictionless newsletter, so without further ado...
-                            </p>
-                        </div><!-- .whitebox_primary_post_content -->
-                        <hr class="solid" />
+                            <hr />
+                            <div class="whitebox_primary_post_attr">
+                                <span class="whitebox_primary_post_attr_item author">Posted by <a href="http://wewillraakyou.com/about/the-founders/gerrie/">Gerrie Smits</a></span>
+                                <span class="seperator">|</span>
+                                <span class="whitebox_primary_post_attr_item date"> 5 Dec 2011</span>
+                                <span class="seperator">|</span>
+                                <!--span class="whitebox_primary_post_attr_item time">12:44</span>
+                                <span class="seperator">|</span-->
+                                <span class="whitebox_primary_post_attr_item comments"><img class="commenticon" src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/images/whitebox_primary_body_attr_comment_icon.png" />0 comments</span>
+                            </div><!-- .whitebox_primary_post_attr -->
+                            <div class="whitebox_primary_post_content">
+                                <div class="whitebox_primary_post_content_right">
+                                    <a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/12/friend_request_from_your_unborn_child.jpg" alt="friend_request_from_your_unborn_child" title="friend_request_from_your_unborn_child"  width="162" height="104" /></a>
+                                    <a class="more_link" href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">More &#9658;</a>
+                                </div><!-- .whitebox_primary_post_content_right -->
+                                <p>
+                                    Later this week the Pulitzer Prize Board will <a href="http://www.pulitzer.org/digital_entries">announce the details</a> of their new journalism prize categories, which will emphasize real-time reporting (that&#39;s live-tweeting to us tweeps). Until then, there's the weekly RAAKonteur.
+                                </p>
+                            </div><!-- .whitebox_primary_post_content -->
+                            <hr class="solid" />
+                        </article>
+                        <article>
+                            <h3><a href="http://wewillraakyou.com/2011/11/the-raakonteur-65-kevin-roses-new-project-the-facebook-freakyline-and-more/">The RAAKonteur #65 - Kevin Rose's new project, The Facebook Freakyline and more</a></h3>
+                            <hr />
+                            <div class="whitebox_primary_post_attr">
+                                <span class="whitebox_primary_post_attr_item author">Posted by <a href="http://wewillraakyou.com/about/the-founders/gerrie/">Gerrie Smits</a></span>
+                                <span class="seperator">|</span>
+                                <span class="whitebox_primary_post_attr_item date">28 Nov 2011</span>
+                                <span class="seperator">|</span>
+                                <!--span id="whitebox_primary_attr_post_time" class="whitebox_primary_post_attr_item">11:59</span>
+                                <span class="seperator">|</span-->
+                                <span class="whitebox_primary_post_attr_item comments"><img class="commenticon" src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/images/whitebox_primary_body_attr_comment_icon.png" />0 comments</span>
+                            </div><!-- .whitebox_primary_post_attr -->
+                            <div class="whitebox_primary_post_content">
+                                <div class="whitebox_primary_post_content_right">
+                                    <a href="http://wewillraakyou.com/2011/11/the-raakonteur-65-kevin-roses-new-project-the-facebook-freakyline-and-more/"><img src="http://wewillraakyou.com/wp-content/uploads/2011/11/Picture_39.png" alt="etsy-recommendations" title="etsy-recommendations"  width="162" height="104" /></a>
+                                    <a class="more_link" href="http://wewillraakyou.com/2011/11/the-raakonteur-65-kevin-roses-new-project-the-facebook-freakyline-and-more/">More &#9658;</a>
+                                </div><!-- .whitebox_primary_post_content_right -->
+                                <p>
+                                    This week we are trying out a frictionless newsletter, so without further ado...
+                                </p>
+                            </div><!-- .whitebox_primary_post_content -->
+                            <hr class="solid" />
+                        </article>
                     </div>
                     <footer class="whitebox_primary_footer box_nav small_arial_caps">
                         <a class="whitebox_primary_footer_left" href="http://wewillraakyou.com/blog-archive/">All blog posts</a>
@@ -112,13 +156,13 @@ Template Name:Home
                         </div><!-- .whitebox_primary_footer_right -->
                     </footer><!-- .whitebox_primary_footer -->
                 </div><!-- #whitebox_primary -->
-            </section><!-- tab_container -->
-            <section class="tab_container whitebox-secondary other_posts">
+            </div><!-- tab_container -->
+            <div class="tab_container whitebox-secondary other_posts">
                 <div class="grey_tab tab tab104 rounded-corners">
                         <h2>Other Posts</h2>
                 </div><!-- .grey_tab -->
                 <div class="whitebox_secondary whitebox box rounded-corners">
-                    <div class="other_posts_content_one">
+                    <section class="other_posts_content_one">
                         <h3 class="small_arial_caps">Must Reads</h3>
                         <ul>
                             <li><a href="http://wewillraakyou.com/2010/12/klout-is-broken/">Klout is broken</a></li>
@@ -130,8 +174,8 @@ Template Name:Home
                         <footer>
                             <a class="more_link" href="http://wewillraakyou.com/category/must-read/" rel="nofollow">More &#9660;</a>
                         </footer>
-                    </div><!-- _content_one -->
-                    <div class="other_posts_content_two">
+                    </section><!-- _content_one -->
+                    <section class="other_posts_content_two">
                         <h3 class="small_arial_caps">Worth a Look</h3>
                         <ul>
                             <li><a href="http://wewillraakyou.com/2011/09/identity-and-location-and-sex-welcome-to-people-discovery/">Identity and location (and sex) - welcome to people discovery </a></li>
@@ -141,8 +185,8 @@ Template Name:Home
                         <footer>
                             <a class="more_link" href="http://wewillraakyou.com/category/worth-a-look/" rel="nofollow">More &#9660;</a>
                         </footer>
-                    </div><!-- #whitebox_secondary_content_two -->
-                    <div class="other_posts_content_three">
+                    </section><!-- content_two -->
+                    <section class="other_posts_content_three">
                         <h3 class="small_arial_caps">The RAAKonteur</h3>
                         <ul>
                             <li><a href="http://wewillraakyou.com/2011/12/the-raakonteur-66-the-spotify-platform-how-people-look-at-your-facebook-profile-wordpress-ads-and-more/">The RAAKonteur #66 - Spotify Platform, How people look at your Facebook profile, Wordpress Ads and more</a></li>
@@ -152,9 +196,9 @@ Template Name:Home
                         <footer>
                             <a class="more_link" href="http://wewillraakyou.com/blog/the-raakonteur/" rel="nofollow">More &#9660;</a>
                         </footer>
-                    </div><!-- #whitebox_secondary_content_three -->
+                    </section><!-- content_three -->
                 </div><!-- #whitebox_secondary -->
-            </section><!-- tab_container -->
+            </div><!-- tab_container -->
         </div><!-- #content -->
 <?php get_sidebar() ?>
 <?php get_footer() ?>
