bcd95c57cfdc126d48c1dfddfd54610b35f93547
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-03 12:39:45 +0200

    removing px from img tag measurement for validation

--- single_product.php
+++ single_product.php
@@ -29,7 +29,7 @@ Template Name:Single Product
             </div><!-- whitebox_primary -->
             <div class="bluebox bluebox_primary box bluebox-primary rounded-corners bluebox_top_margin">
                 <div class="bluebox_bigpic">
-                    <img width="315px" height="203px" src="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder01.png">
+                    <img width="315" height="203" src="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder01.png">
                 </div>
                 <hr class="solid blue_hr" />
                 <div class="bluebox_thumbpic">
