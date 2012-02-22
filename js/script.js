42761b858a6e606e259ce83515279d432f45a12c
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-22 13:34:26 +0200

    editing logo js and removing extra letters

--- js/script.js
+++ js/script.js
@@ -5,27 +5,27 @@ letters[2] =['137f63eaec658cecd6305fa150be320d.jpg', '0387e51d17f01d524c0b07de14
 letters[3] = ['0c71b1cbe12da4fdbe88df63f21dbfce.jpg', '78ce302c784ece3f208eba8152c2bc58.jpg', '94f1cb321eb37b70e6f4a789514aee55.jpg'];
 
 var load_next = function() {
-    var letter = (Math.round(Math.random()*4)) - 1;
+    var letter = (Math.floor(Math.random()*4));
     console.log('letter:' + letter);
     switch (letter)
     {
         case 1:
-            var variant = Math.round(Math.random()*((letters[letter]).length));
+            var variant = Math.floor(Math.random()*(((letters[letter]).length)+1));
             console.log('variant:' + variant);
             $('#logo_letter_r1 img').attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/logo_letters/r/' + letters[letter][variant]);
             break;
         case 2:
-            var variant = Math.round(Math.random()*((letters[letter]).length));
+            var variant = Math.floor(Math.random()*(((letters[letter]).length)+1));
             console.log('variant:' + variant);
             $('#logo_letter_a2 img').attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/logo_letters/a/' + letters[letter][variant]);
             break;
         case 3:
-            var variant = Math.round(Math.random()*((letters[letter]).length));
+            var variant = Math.floor(Math.random()*(((letters[letter]).length)+1));
             console.log('variant:' + variant);
             $('#logo_letter_a3 img').attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/logo_letters/a/' + letters[letter][variant]);
             break;
         case 4:
-            var variant = Math.round(Math.random()*((letters[letter]).length));
+            var variant = Math.floor(Math.random()*(((letters[letter]).length)+1));
             console.log('variant:' + variant);
             $('#logo_letter_k4 img').attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/logo_letters/k/' + letters[letter][variant]);
             break;
