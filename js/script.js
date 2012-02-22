39eb81e6d1948f1fda7110eecbdf9372657d540d
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-22 13:50:17 +0200

    editing logo js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,19 @@ letters[2] =['137f63eaec658cecd6305fa150be320d.jpg', '0387e51d17f01d524c0b07de14
 letters[3] = ['0c71b1cbe12da4fdbe88df63f21dbfce.jpg', '78ce302c784ece3f208eba8152c2bc58.jpg', '94f1cb321eb37b70e6f4a789514aee55.jpg'];
 
 var load_next = function() {
-    var letter = (Math.floor(Math.random()*4));
-    console.log('letter:' + letter);
-    var variant = Math.floor(Math.random()*(((letters[letter]).length)+1));
+    var letter;
+    var letterNumber = (Math.floor(Math.random()*4));
+    console.log('letter:' + letterNumber);
+    var variant = Math.floor(Math.random()*(((letters[letterNumber]).length)+1));
     console.log('variant:' + variant);
-    $('#logo_letter_' + variant + ' img').attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/logo_letters/k/' + letters[letter][variant]);
+    if (variant === 0) {
+        letter = 'r';
+    } else if (variant === 3) {
+        letter = 'k';
+    } else {
+        letter = 'a';
+    }
+    $('#logo_letter_' + (letterNumber + 1) + ' img').attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/logo_letters/' + letter  + '/' + letters[letterNumber][variant]);
     setTimeout(load_next, 3000);
 }
 $(document).ready(function() {
