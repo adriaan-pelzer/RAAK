e17b002d0a8c113d92b9b4624c26a6829efdb888
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-23 15:07:58 +0200

    adding short code for logo container on home

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function logo_call_to_action() {
     $logo_story = get_page_by_title('the-perpetually-changing-crowdsourced-raak-logo');
-    echo $logo_story;
+    print_r($logo_story);
     $logo_container = '<aside id="logox_counter" class="rounded-corners din-schrift">
     <span class="point_left"></span>
     <span class="point_right"></span>
@@ -10,7 +10,7 @@ function logo_call_to_action() {
         <h2># OF LOGO COMBINATIONS</h2>
     </header>
     <div id="logox_counter_number">7744</div>
-    <a id="read_the_logo_story" href="' + get_page_link($logo_story -> ID) + '">Read the story behind our logo</a>
+    <a id="read_the_logo_story" href="' + echo get_bloginfo ('url') +'/2010/11/the-perpetually-changing-crowdsourced-raak-logo/">Read the story behind our logo</a>
     <hr>
     <a id="upload_a_letter" href="' + get_bloginfo ('template_directory') + '/logo-project/">Upload a letter</a>
     </aside>';
