62d25f29a247c784c83062046e81318559e9a82c
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-23 15:29:31 +0200

    adding short code for logo container on home

--- functions.php
+++ functions.php
@@ -1,8 +1,5 @@
 <?php
-global $wpdb;
 function logo_call_to_action() {
-    $logo_story = get_page_by_title('The perpetually changing crowdsourced RAAK logo');
-    print_r($logo_story);
     $logo_container = '<aside id="logox_counter" class="rounded-corners din-schrift">
     <span class="point_left"></span>
     <span class="point_right"></span>
