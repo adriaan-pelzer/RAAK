eaaefdd0c62f3a1ba636e5c7d129db57019c553b
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-23 15:16:52 +0200

    adding short code for logo container on home

--- functions.php
+++ functions.php
@@ -8,9 +8,9 @@ function logo_call_to_action() {
         <h2># OF LOGO COMBINATIONS</h2>
     </header>
     <div id="logox_counter_number">7744</div>
-    <a id="read_the_logo_story" href="' + get_bloginfo ('url') +'/2010/11/the-perpetually-changing-crowdsourced-raak-logo/">Read the story behind our logo</a>
+    <a id="read_the_logo_story" href="http://wewillraakyou.com/2010/11/the-perpetually-changing-crowdsourced-raak-logo/">Read the story behind our logo</a>
     <hr>
-    <a id="upload_a_letter" href="' + get_bloginfo ('template_directory') + '/logo-project/">Upload a letter</a>
+    <a id="upload_a_letter" href="http://wewillraakyou.com/wp-content/themes/RAAK/logo-project/">Upload a letter</a>
     </aside>';
     return $logo_container;
 }
