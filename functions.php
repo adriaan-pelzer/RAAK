c6cd6887650fb0029cebdf109603770ac4bea203
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-23 15:52:12 +0200

    generating links

--- functions.php
+++ functions.php
@@ -1,8 +1,7 @@
 <?php
 global $wpdb;
 function logo_call_to_action() {
-    $logo_story = get_page_by_title('Logo Project');
-    print_r($logo_story);
+    $logo_project = get_page_by_title('Logo Project');
     $logo_container = '<aside id="logox_counter" class="rounded-corners din-schrift">
     <span class="point_left"></span>
     <span class="point_right"></span>
@@ -12,7 +11,7 @@ function logo_call_to_action() {
     <div id="logox_counter_number">7744</div>
     <a id="read_the_logo_story" href="http://wewillraakyou.com/2010/11/the-perpetually-changing-crowdsourced-raak-logo/">Read the story behind our logo</a>
     <hr>
-    <a id="upload_a_letter" href="http://wewillraakyou.com/wp-content/themes/RAAK/logo-project/">Upload a letter</a>
+    <a id="upload_a_letter" href="' + get_permaink($logo_story=>ID) + '">Upload a letter</a>
     </aside>';
     return $logo_container;
 }
