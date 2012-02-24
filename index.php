18ffe47003142e335517f05f699e380babbe6342
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-24 15:45:43 +0200

    adding short code for our work container on home

--- index.php
+++ index.php
@@ -7,83 +7,8 @@ Template Name:Home
 
     <div class="container rounded-corners home">
         <div class="content">
-<?php echo do_shortcode('[logo_cta]'); ?>
-            <div id="bluebox_home_left" class="bluebox box rounded-corners">
-                <header>
-                    <h2 class="din-schrift"><a href="http://wewillraakyou.com/our-work/">Our Work</a></h2>
-                </header>
-                <hr />    
-                <p>RAAK is a social &amp; digital media <em>plug-in</em>. We put you in touch with people, be it your customers, your suppliers, or employees.</p>
-            </div><!-- #bluebox_home_left -->
-            <div id="bluebox_home_right" class="bluebox bluebox_primary_no_margin box rounded-corners">
-                    <nav class="bluebox_nav box_nav">
-                        <h3 class="bluebox_nav_item small_arial_caps"><a onclick="javascript: expand('social-media-ready');">social media ready</a></h3>
-                        <span class="seperator">|</span>
-                        <h3 class="bluebox_nav_item small_arial_caps"><a onclick="javascript: expand('impact-projects');">impact projects</a></h3>
-                        <span class="seperator">|</span>
-                        <h3 class="bluebox_nav_item small_arial_caps"><a class="active" onclick="javascript: expand('consulting');">Consulting</a></h3>
-                    </nav>
-                    <section class="bluebox_content" id="social-media-ready" style="display: none;">
-                        <a href="http://wewillraakyou.com/2010/06/social-media-strategy-for-the-world-economic-forum/"><img alt="" src="http://wewillraakyou.com/wp-content/gallery/wef/davos-2.png"  width="315" /></a>
-                        <ul>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Client:</span>
-                                <span class="title">World Economic Forum</span>
-                            </li>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Project:</span>
-                                <span class="title">WELCOM</span>
-                            </li>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Overview:</span>
-                                <span class="overview">A strategy & functional specification for a social network for the world's most influential people. <br /><strong>Read More >></strong></span>
-                            </li>
-                            <li class="bluebox_content_link">
-                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
-                            </li>
-                        </ul>
-                    </section>
-                    <section class="bluebox_content" id="impact-projects" style="display: none;">
-                        <a href="http://wewillraakyou.com/2010/06/social-media-strategy-for-the-world-economic-forum/"><img alt="" src="http://wewillraakyou.com/wp-content/gallery/wef/davos-2.png"  width="315" /></a>
-                        <ul>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Client:</span>
-                                <span class="title">World Economic Forum</span>
-                            </li>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Project:</span>
-                                <span class="title">WELCOM</span>
-                            </li>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Overview:</span>
-                                <span class="overview">A strategy & functional specification for a social network for the world's most influential people. <br /><strong>Read More >></strong></span>
-                            </li>
-                            <li class="bluebox_content_link">
-                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
-                            </li>
-                        </ul>
-                    </section>
-                    <section class="bluebox_content" id="consulting">
-                        <a href="http://wewillraakyou.com/2010/06/social-media-strategy-for-the-world-economic-forum/"><img alt="" src="http://wewillraakyou.com/wp-content/gallery/wef/davos-2.png"  width="315" /></a>
-                        <ul>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Client:</span>
-                                <span class="title">World Economic Forum</span>
-                            </li>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Project:</span>
-                                <span class="title">WELCOM</span>
-                            </li>
-                            <li class="bluebox_content_sub">
-                                <span class="label">Overview:</span>
-                                <span class="overview">A strategy & functional specification for a social network for the world's most influential people. <br /><strong>Read More >></strong></span>
-                            </li>
-                            <li class="bluebox_content_link">
-                                <a href="http://wewillraakyou.com/our-work/?category=consulting" rel="nofollow">More Projects &#9660;</a>
-                            </li>
-                        </ul>
-                    </section>
-            </div><!-- #bluebox_home_right -->
+<?php do_shortcode('[logo_cta]'); ?>
+<?php do_shortcode('[OW_home]'); ?>
 <?php do_shortcode('[latest_posts]'); ?>
             <div class="tab_container whitebox-secondary other_posts">
                 <div class="grey_tab tab tab104 rounded-corners">
