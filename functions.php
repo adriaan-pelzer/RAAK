aba12c626e610dba1406fc1a21033cc1566ba3cd
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-27 13:12:32 +0200

    debugging adding dynamic our work blue box

--- functions.php
+++ functions.php
@@ -123,9 +123,9 @@ function our_work_home_blue()
         $our_work_bluebox_content .= '<a href="' . get_permalink($current_our_work_post_id) . '">';
         $our_work_bluebox_content .= get_image_or_video ($current_our_work_post->post_content, 315) . '</a>';
         $our_work_bluebox_content .= '<ul><li class="bluebox_content_sub"><span class="label">Client:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Client', true) . '</span></li>';
-        $our_work_bluebox_content .= '<ul><li class="bluebox_content_sub"><span class="label">Project:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Project', true) . '</span></li>';
-        $our_work_bluebox_content .= '<ul><li class="bluebox_content_sub"><span class="label">Overview:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Overview', true) . '</span></li>';
-        $our_work_bluebox_content .= '<ul><li class="bluebox_content_link"><a href="' . get_category_link($current_our_work_post_cat) . '" rel="nofollow">More Projects &#9660;</a></li></ul></section>';
+        $our_work_bluebox_content .= '<li class="bluebox_content_sub"><span class="label">Project:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Project', true) . '</span></li>';
+        $our_work_bluebox_content .= '<li class="bluebox_content_sub"><span class="label">Overview:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Overview', true) . '</span></li>';
+        $our_work_bluebox_content .= '<li class="bluebox_content_link"><a href="' . get_category_link($current_our_work_post_cat) . '" rel="nofollow">More Projects &#9660;</a></li></ul></section>';
     }
 ?>
         </nav>
