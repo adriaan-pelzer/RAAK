8258ee218a459d17116ff0d4136c48bd4366ae1d
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-27 16:13:43 +0200

    using wp query object instead of get posts for our work on home

--- functions.php
+++ functions.php
@@ -125,7 +125,7 @@ function our_work()
         }
         $our_work_bluebox_content .= '" id="' . $work_category->category_nicename . '">';
         $our_work_bluebox_content .= '<a href="' . get_permalink($current_our_work_post_id) . '">';
-        $our_work_bluebox_content .= get_image_or_video ($current_our_work_post[0]->post_content, 315) . '</a>';
+        $our_work_bluebox_content .= get_image_or_video ($current_our_work_post->post_content, 315) . '</a>';
         $our_work_bluebox_content .= '<ul><li class="bluebox_content_sub"><span class="label">Client:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Client', true) . '</span></li>';
         $our_work_bluebox_content .= '<li class="bluebox_content_sub"><span class="label">Project:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Project', true) . '</span></li>';
         $our_work_bluebox_content .= '<li class="bluebox_content_sub"><span class="label">Overview:</span><span class="title">' . get_post_meta ($current_our_work_post_id, 'Overview', true) . '</span></li>';
