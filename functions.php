d238d42111e089235853c24cf96ff12939be4cd2
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-28 10:46:42 +0200

    debugging number of posts displaying on home

--- functions.php
+++ functions.php
@@ -42,6 +42,7 @@ function display_latest_posts($atts) {
             $latest_posts_loop->the_post();
             $author_full_name = get_the_author_meta('first_name') . ' ' . get_the_author_meta('last_name');
             $author_page = get_page_by_title($author_full_name);
+            the_title();
 ?>
 <?php
         }
