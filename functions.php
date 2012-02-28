160981fb8b15851edf8daa4673635ed220799581
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-28 13:47:28 +0200

    making other posts dynamic

--- functions.php
+++ functions.php
@@ -123,7 +123,7 @@ function our_work()
 <?php
         $current_our_work_post_cat = get_cat_id ($work_category->name);
         $current_our_work_query = new WP_Query('cat=' . get_cat_id($work_category->name) .'&posts_per_page=1$paged=1');
-        $current_our_work_post = $current_our_work_query->post;
+        $current_our_work_post = $current_our_work_query->posts;
         $current_our_work_post_id = ($current_our_work_post->ID);
         $our_work_bluebox_content .= '<section class="bluebox_content our_work_bluebox_content';
         if($cat_number == 0){
