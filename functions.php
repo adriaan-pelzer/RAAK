76c925e4b26d1599c6a48dd76aa84fca72803b7a
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-02-29 16:29:12 +0200

    adding short code and function for blue box on about

--- functions.php
+++ functions.php
@@ -307,32 +307,36 @@ add_shortcode('who_what', 'who_we_are_what_we_do');
 function founder_quotes() {
 ?>
 <aside class="bluebox bluebox_primary box rounded-corners">
-    <header>
+                <header>
+                    <h3 class="box_nav_no_title bluebox_primary_nav box_nav smaller_arial_caps">What we do</h3>
+                </header>
+                <hr class="blue_hr">
+                <div id="bluebox_content_what-we-do" class="bluebox_content_item current">
+                    If advertising is a tax on mediocrity, you've come to a tax free zone.
+                </div><!-- bluebox_content_item -->
 <?php
-    $about = get_page_by_title('About');
-    $about_pages = new WP_Query('post_parent=' . $about->ID . '&post_type=page&meta_key=excerpt');
-        while ($about_pages->have_posts()) {
-            $about_pages->the_post();
+    $who_we_are = get_page_by_title('Who we are');
+    global $post;
+    $who_we_are_pages = new WP_Query('post_parent=' . $who_we_are->ID . '&post_type=page&meta_key=excerpt');
+        while ($who_we_are_pages->have_posts()) {
+            $who_we_are_pages->the_post();
+            print_r $post;
 ?>
-        <h3 class="box_nav_no_title bluebox_primary_nav box_nav smaller_arial_caps"><?php the_title(); ?></h3>
+                <div id="bluebox_content_<?php echo $post->post_name; ?>" class="bluebox_content_item">
+                </div><!-- bluebox_content_item -->
 <?php
 
         }
 ?>
-                </header>
-                <hr class="blue_hr">
-                <div id="bluebox_content_what-we-do" class="bluebox_content_item">
-                    If advertising is a tax on mediocrity, you've come to a tax free zone.
-                </div><!-- bluebox_content_item -->
-                <div id="bluebox_content_adriaan" class="bluebox_content_item" style="display: none;">
+                <!--div id="bluebox_content_adriaan" class="bluebox_content_item" style="display: none;">
                     I dream code.<br>I write machine poetry, that makes electrons dance.<br>
-                </div><!-- bluebox_content_item -->
+                </div>
                 <div id="bluebox_content_wessel" class="bluebox_content_item" style="display: none;">
                     I breathe media.<br>I inhale news and exhale content.<br>Marked up, tagged and loaded.
-                </div><!-- bluebox_content_item -->
+                </div>
                 <div id="bluebox_content_gerrie" class="bluebox_content_item" style="display: none;">
                     I'm intrigued by innovation.<br>Not very interested in the status quo.<br>
-                </div><!-- bluebox_content_item -->
+                </div>< bluebox_content_item -->
             </aside>
 <?php
 }
