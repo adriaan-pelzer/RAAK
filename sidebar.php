db67ab019e6834dbf8154f19a14335a60e14cf9a
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-03-01 12:13:50 +0200

    making twitter feed live

--- sidebar.php
+++ sidebar.php
@@ -172,7 +172,7 @@ function mce_success_cb(resp){
                       rpp: 4,
                       interval: 30000,
                       width: 181,
-                      height: 300,
+                      height: auto,
                       theme: {
                         shell: {
                           background: '#bebebe',
