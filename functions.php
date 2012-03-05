457cba32dc89ca16dcf74e667830da71072d364e
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-03-05 16:42:57 +0200

    changes to our products shortcode

--- functions.php
+++ functions.php
@@ -528,117 +528,133 @@ function big_whitebox($atts) {
 ?>
         </nav>
     </header>
-    <hr>
-    <a class="whitebox_big_category_entry_content" href="http://stage.wewillraakyou.com/our-products-2/single-product/">
-        <div id="post_all-products_0_picture" class="whitebox_big_category_entry_content_picture">
-            <img alt="" src="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder00.png" width="220" height="142">                     </div><!-- #post_0_picture -->
-        <div id="post_all-products_0_overview" class="whitebox_big_category_entry_content_overview" style="width: 160px; height: 82px; display: none;">
-            <p>Rewinder allows you to rewind your Twitter Timeline, or recorded hashtags, as if it was time-shifted TV. Works very well with ... well ... time-shifted TV.</p><p><strong>Read More »</strong></p>
-        </div><!-- #post_0_overview -->
-    </a>
-    </div><!-- .whitebox_big_category_entry -->
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label">Product:</span>
-            <span class="whitebox_big_category_entry_title_name">WOOS.at</span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <hr class="solid">
-    <a class="whitebox_big_category_entry_content" href="http://wewillraakyou.com/2011/11/woos-at-our-app-for-social-serendipity/" onmouseover="javascript: mouse_action ('all-products', 1, 'over');" onmouseout="javascript: mouse_action ('all-products', 1, 'out');">
-        <div id="post_all-products_1_picture" class="whitebox_big_category_entry_content_picture">
-            <img alt="" src="http://wewillraakyou.com/wp-content/gallery/woos-at/woosat00.jpg" width="220" height="142">
-        </div><!-- #post_1_picture -->
-        <div id="post_all-products_1_overview" class="whitebox_big_category_entry_content_overview" style="width: 160px; height: 82px; display: none;">
-            <p>WOOS.at is an app that shows you who else is where you are, and why they might be interesting to you</p><p><strong>Read More »</strong></p>
-        </div><!-- #post_1_overview -->
-    </a>
-    </div><!-- .whitebox_big_category_entry -->
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label"></span>
-            <span class="whitebox_big_category_entry_title_name"></span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_2_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    </div><!-- #whitebox_big_category_row1 -->
-    <div id="whitebox_big_category_row2" class="whitebox_big_category_row">
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label"></span>
-            <span class="whitebox_big_category_entry_title_name"></span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_3_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    <div class="whitebox_big_category_entry">
-    <header>
-            <h3 class="whitebox_big_category_entry_title">
-                <span class="whitebox_big_category_entry_title_label"></span>
-                <span class="whitebox_big_category_entry_title_name"></span>
-            </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_4_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label"></span>
-            <span class="whitebox_big_category_entry_title_name"></span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_5_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    </div><!-- #whitebox_big_category_row2 -->
-    <div id="whitebox_big_category_row3" class="whitebox_big_category_row">
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label"></span>
-            <span class="whitebox_big_category_entry_title_name"></span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_6_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label"></span>
-            <span class="whitebox_big_category_entry_title_name"></span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_7_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    <div class="whitebox_big_category_entry">
-    <header>
-        <h3 class="whitebox_big_category_entry_title">
-            <span class="whitebox_big_category_entry_title_label"></span>
-            <span class="whitebox_big_category_entry_title_name"></span>
-        </h3><!-- .whitebox_big_category_entry_title -->
-    </header>
-    <!--div id="post_8_picture" class="whitebox_big_category_entry_content_picture">
-        <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
-    </div-->
-    </div><!-- .whitebox_big_category_entry -->
-    </div><!-- #whitebox_big_category_row3 -->
-    </div><!-- #whitebox_big_all-products_1 -->
-    </div><!-- #whitebox_big_all-products -->
-    </div>
+    <hr />
+    <div id="whitebox_big_all_items" class="whitebox_big_category">
+                    <div class="whitebox_big_category_page smaller_arial_caps">
+                        <a id="whitebox_nav_1" class="whitebox_big_category_page_item active" onclick="javascript: page('all-products', 1, 1);">1</a>
+                        <a class="whitebox_big_category_page_item_arrow" onclick="javascript: page('all-products', current_page + 1, 1);">►</a>
+                    </div><!-- .whitebox_big_category_page -->
+                    <hr class="solid">
+                    <div id="whitebox_big_all-products_1" class="whitebox_big_category smaller_arial_caps">
+                        <div id="whitebox_big_category_row1" class="whitebox_big_category_row">
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label">Product:</span>
+                                        <span class="whitebox_big_category_entry_title_name">Rewinder</span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <hr class="solid">
+                                <a class="whitebox_big_category_entry_content" href="http://stage.wewillraakyou.com/our-products-2/single-product/" onmouseover="javascript: mouse_action ('all-products', 0, 'over');" onmouseout="javascript: mouse_action ('all-products', 0, 'out');">
+                                    <div id="post_all-products_0_picture" class="whitebox_big_category_entry_content_picture">
+                                        <img alt="" src="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder00.png" width="220" height="142">                     </div><!-- #post_0_picture -->
+                                    <div id="post_all-products_0_overview" class="whitebox_big_category_entry_content_overview" style="width: 160px; height: 82px; display: none;">
+                                        <p>Rewinder allows you to rewind your Twitter Timeline, or recorded hashtags, as if it was time-shifted TV. Works very well with ... well ... time-shifted TV.</p><p><strong>Read More »</strong></p>
+                                    </div><!-- #post_0_overview -->
+                                </a>
+                            </div><!-- .whitebox_big_category_entry -->
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label">Product:</span>
+                                        <span class="whitebox_big_category_entry_title_name">WOOS.at</span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <hr class="solid">
+                                <a class="whitebox_big_category_entry_content" href="http://wewillraakyou.com/2011/11/woos-at-our-app-for-social-serendipity/" onmouseover="javascript: mouse_action ('all-products', 1, 'over');" onmouseout="javascript: mouse_action ('all-products', 1, 'out');">
+                                    <div id="post_all-products_1_picture" class="whitebox_big_category_entry_content_picture">
+                                        <img alt="" src="http://wewillraakyou.com/wp-content/gallery/woos-at/woosat00.jpg" width="220" height="142">
+                                    </div><!-- #post_1_picture -->
+                                    <div id="post_all-products_1_overview" class="whitebox_big_category_entry_content_overview" style="width: 160px; height: 82px; display: none;">
+                                        <p>WOOS.at is an app that shows you who else is where you are, and why they might be interesting to you</p><p><strong>Read More »</strong></p>
+                                    </div><!-- #post_1_overview -->
+                                </a>
+                            </div><!-- .whitebox_big_category_entry -->
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label"></span>
+                                        <span class="whitebox_big_category_entry_title_name"></span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_2_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                        </div><!-- #whitebox_big_category_row1 -->
+                        <div id="whitebox_big_category_row2" class="whitebox_big_category_row">
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label"></span>
+                                        <span class="whitebox_big_category_entry_title_name"></span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_3_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                        <h3 class="whitebox_big_category_entry_title">
+                                            <span class="whitebox_big_category_entry_title_label"></span>
+                                            <span class="whitebox_big_category_entry_title_name"></span>
+                                        </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_4_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label"></span>
+                                        <span class="whitebox_big_category_entry_title_name"></span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_5_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                        </div><!-- #whitebox_big_category_row2 -->
+                        <div id="whitebox_big_category_row3" class="whitebox_big_category_row">
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label"></span>
+                                        <span class="whitebox_big_category_entry_title_name"></span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_6_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label"></span>
+                                        <span class="whitebox_big_category_entry_title_name"></span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_7_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                            <div class="whitebox_big_category_entry">
+                                <header>
+                                    <h3 class="whitebox_big_category_entry_title">
+                                        <span class="whitebox_big_category_entry_title_label"></span>
+                                        <span class="whitebox_big_category_entry_title_name"></span>
+                                    </h3><!-- .whitebox_big_category_entry_title -->
+                                </header>
+                                <!--div id="post_8_picture" class="whitebox_big_category_entry_content_picture">
+                                    <img src="http://wewillraakyou.com/wp-content/themes/RAAK/images/default_post.jpg" />
+                                </div-->
+                            </div><!-- .whitebox_big_category_entry -->
+                        </div><!-- #whitebox_big_category_row3 -->
+                    </div><!-- #whitebox_big_all-products_1 -->
+                </div><!-- #whitebox_big_all-products -->
+            </div>
 <?php
 }
 
