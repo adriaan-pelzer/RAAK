5205edbb8fcaca4147dc110b40f5f01a3aa93ca4
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-03-05 11:20:36 +0200

    pulling uploaded letters from database

--- functions.php
+++ functions.php
@@ -378,6 +378,7 @@ $uploads = $wpdb->get_results ('SELECT * FROM `wp_logo_uploads` ORDER BY `timest
 print_r($uploads);
 foreach($uploads as $upload) {
     print_r($upload);
+}
 ?>
 <div class="bluebox logo_project_bluebox_primary bluebox_primary box rounded-corners">
                 <header>
