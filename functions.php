dac3bd0e28ce13bf0ce4bd8dfa3dd921cceba68b
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-03-05 18:12:11 +0200

    changes to our products shortcode

--- functions.php
+++ functions.php
@@ -540,7 +540,7 @@ function big_whitebox($atts) {
                     <hr class="solid">
 <?php
     $item_count = 0;
-    for($page_count = 0; $page_count < $total_pages; $page_count) {
+    for($page_count = 0; $page_count < $total_pages; $page_count++) {
         echo $page_count;
        echo $total_pages; 
 ?>
