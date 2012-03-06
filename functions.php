5aca252ac7eda48b46f064d7ffbf439c71cf4a41
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-03-06 16:16:03 +0200

    editing function for single project

--- functions.php
+++ functions.php
@@ -602,7 +602,7 @@ function single_project_whitebox() {
         while(have_posts()) {
             the_post();
             $cats = get_the_category();
-            if (in_array('RAAK Products', $cats) {
+            if (in_array('RAAK Products', $cats)) {
                 echo 'yes';
             }
 
