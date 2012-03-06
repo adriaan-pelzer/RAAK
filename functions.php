fb09a4ff45d90731e6f6e6096b0468cdb2cfed6e
Adriaan Pelzer <adriaan@dragon.pitproduksies.org>
2012-03-06 15:49:50 +0200

    adding shortcode and function for single projects

--- functions.php
+++ functions.php
@@ -592,6 +592,36 @@ function big_whitebox($atts) {
 }
 
 add_shortcode('bwb', 'big_whitebox');
+
+/*************************/
+
+function single_project_whitebox() {
+?>
+
+<div class="whitebox_big whitebox box big_box big_box_short rounded-corners_top_bottom_right">
+    <header>
+        <h2  class="din-schrift blue_20">Our Products</h2>
+    </header>
+        <hr />
+</div><!-- whitebox_big -->
+<div class="whitebox whitebox_primary whitebox-primary box rounded-corners_bottom">
+    <div class="product_whitebox_primary_project whitebox_primary_section bigger_arial_caps">
+        <div class="whitebox_primary_section_label">Product:</div>
+        <div class="whitebox_primary_section_content"><a class="grey_text" target="_blank" href="http://m.tweetrewinder.com">Rewinder</a></div>
+    </div><!-- whitebox_primary_project -->
+    <hr class="solid thicker_hr lightblue_hr">
+    <div class="whitebox_primary_solution whitebox_primary_section">
+        <div class="whitebox_primary_section_label bigger_arial_caps">Descript:</div>
+        <div class="whitebox_primary_section_content"><p>Rewinder is our mobile solution to make Twitter useful with Time-shifted TV</p>
+            <p>Connect your Twitter account, and we'll allow you to view your past Twitter timeline, as well as recorded hashtags, as if they were happening right now!</p>
+            <p>If you're just quickly catching up with an event, you can play them back at faster than realtime too.</p>
+            <p>This is as close to Time Travel you'll ever get, without altering the future!</p></div>
+        </div><!-- .whitebox_primary_solution -->
+</div><!-- whitebox_primary -->
+<?php
+}
+
+add_shortcode('sp_wb', 'single_project_whitebox');
 ?>
 <?php 
 /***************************** From local functions on old site **************************/
--- single_product.php
+++ single_product.php
@@ -7,26 +7,7 @@ Template Name Posts: Single Product
 
 	<div class="container rounded-corners single_product">
 		<div class="content">
-            <div class="whitebox_big whitebox box big_box big_box_short rounded-corners_top_bottom_right">
-                <header>
-                    <h2  class="din-schrift blue_20">Our Products</h2>
-                </header>
-                    <hr />
-            </div><!-- whitebox_big -->
-            <div class="whitebox whitebox_primary whitebox-primary box rounded-corners_bottom">
-                    <div class="product_whitebox_primary_project whitebox_primary_section bigger_arial_caps">
-                        <div class="whitebox_primary_section_label">Product:</div>
-                        <div class="whitebox_primary_section_content"><a class="grey_text" target="_blank" href="http://m.tweetrewinder.com">Rewinder</a></div>
-                    </div><!-- whitebox_primary_project -->
-                    <hr class="solid thicker_hr lightblue_hr">
-                    <div class="whitebox_primary_solution whitebox_primary_section">
-                        <div class="whitebox_primary_section_label bigger_arial_caps">Descript:</div>
-                        <div class="whitebox_primary_section_content"><p>Rewinder is our mobile solution to make Twitter useful with Time-shifted TV</p>
-<p>Connect your Twitter account, and we'll allow you to view your past Twitter timeline, as well as recorded hashtags, as if they were happening right now!</p>
-<p>If you're just quickly catching up with an event, you can play them back at faster than realtime too.</p>
-<p>This is as close to Time Travel you'll ever get, without altering the future!</p></div>
-                    </div><!-- .whitebox_primary_solution -->
-            </div><!-- whitebox_primary -->
+<?php do_shortcode('[sp_wb]'); ?>
             <div class="bluebox bluebox_primary box bluebox-primary rounded-corners bluebox_top_margin">
                 <div class="bluebox_bigpic">
                     <img alt="rewinder01" width="315" height="203" src="http://wewillraakyou.com/wp-content/gallery/rewinder/rewinder01.png">
