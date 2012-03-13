76557fb3b106a253d61d66f1745a908a6525d15b
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-13 11:19:07 +0200

    testing tabs in cat box

--- blog.php
+++ blog.php
@@ -10,7 +10,7 @@ Template Name: Blog
 <?php do_shortcode('[sb]'); ?>
 <?php do_shortcode('[latest_posts category="Blog" posts_per_page="2" num_pages="10" tab="0" ]'); ?>
 <?php do_shortcode('[other_posts category1="Must Read" category2="Worth a look" category3="RAAKonteur" colourscheme="blue" ]'); ?>
-<?php do_shortcode('[cat_box tab1="mostviewed" tab2="category"]'); ?>
+<?php do_shortcode('[cat_box tab1="most-viewed" tab2="category" ]'); ?>
         </div><!-- #content -->
 <?php get_sidebar() ?>
 <?php get_footer() ?>
