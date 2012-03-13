537851d23f8127313ef7a1740f6d5563dc19fe4a
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-13 16:29:41 +0200

    modifying index so all it does is include home.php and adding comments.php again

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-/*
-Template Name:Home 
- */
+include('home.php');
 ?>
-<?php get_header() ?>
-
-    <div class="container rounded-corners home">
-        <div class="content">
-<?php do_shortcode('[logo_cta]'); ?>
-<?php do_shortcode('[our_work]'); ?>
-<?php do_shortcode('[latest_posts category="Blog" posts_per_page="2" num_pages="10" tab="1" ]'); ?>
-<?php do_shortcode('[other_posts category1="Must Read" category2="Worth a look" category3="RAAKonteur" colourscheme="white" ]'); ?>
-        </div><!-- #content -->
-<?php get_sidebar() ?>
-<?php get_footer() ?>
