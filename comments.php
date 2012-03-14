eb3d25ad35adf5383b72811e4d1389a26800a822
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-14 11:03:13 +0200

    adding comment form to comments.php

--- comments.php
+++ comments.php
@@ -59,9 +59,10 @@ if (have_comments()) {
 ?>
 <?php
 if ( 'open' == $post->comment_status ) {
-    populate_options();
     do_action('populate_options');
-    $req = get_option('require_name_email'); // Checks if fields are required. Thanks, Adam. ;-) ?>
+    $req = get_option('require_name_email'); // Checks if fields are required. Thanks, Adam. ;-)
+    echo $req
+?>
 
                 <div id="whitebox_primary_comments">
                     <header>
