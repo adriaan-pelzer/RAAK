ba767c2d259995c5f3859472346d2d7646922b23
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-14 17:22:09 +0200

    adding authors box on blog archive

--- functions.php
+++ functions.php
@@ -1203,6 +1203,33 @@ function blog_tag_box() {
 <?php
 }
 add_shortcode('tags', 'blog_tag_box');
+
+/*************************/
+
+function post_authors() {
+    $authors = get_users(array('who' => 'authors'));
+    print_r($authors);
+?>
+<div class="whitebox-secondary tab_container">
+                <div class="blue_tab tab tab108 rounded-corners">
+                    <header>
+                        <h2>Authors</h2>
+                    </header>
+                </div><!-- blue_tab -->
+                <div class="whitebox_secondary blog_whitebox_secondary whitebox box rounded-corners">
+                    <ul>
+                        <!--li></li-->      
+                        <li><a href="http://wewillraakyou.com/author/adriaan/">Adriaan Pelzer</a></li>      
+                        <!--li></li-->      
+                        <li><a href="http://wewillraakyou.com/author/gerrie/">Gerrie Smits</a></li>      
+                        <!--li></li-->      
+                        <li><a href="http://wewillraakyou.com/author/wessel/">Wessel van Rensburg</a></li>      
+                    </ul>
+                </div><!-- whitebox_secondary -->
+            </div>
+<?php
+}
+add_shortcode ('authors', 'post_authors');
 ?>
 <?php 
 
