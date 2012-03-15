1a905eb94c7e2886332460e76c75c4b846aeef21
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-15 20:16:31 +0200

    working on logo project letters

--- functions.php
+++ functions.php
@@ -388,7 +388,7 @@ add_shortcode('wblp', 'whitebox_logo_project');
 
 function logo_project_latest_uploads() {
 
-    $result = mysql_query('SELECT * FROM *');
+    $result = mysql_query('SELECT * FROM table');
     print_r($result);
 
 ?>
