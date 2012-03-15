2c7716133fae9c7e4298ab4b79a4c4e0140f6baf
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-15 21:18:06 +0200

    working on logo project letters

--- functions.php
+++ functions.php
@@ -389,7 +389,7 @@ add_shortcode('wblp', 'whitebox_logo_project');
 function logo_project_latest_uploads() {
 
 global $wpdb;
-    $result = $wpdb->get_results("SELECT * FROM $wpdb");
+    $result = $wpdb->get_results("SELECT * FROM $wpdb-> *");
     
     
     print_r($result);
