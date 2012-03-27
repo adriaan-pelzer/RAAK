521501ff587e6072b1d9ba210e5ba2027e433488
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-27 13:08:33 +0200

    updating latest uploads generation

--- functions.php
+++ functions.php
@@ -409,7 +409,7 @@ function logo_project_latest_uploads() {
     foreach ($different_letters as $letter) {
 ?>
         <span class="logo_project_bluebox_nav_item">
-        <span class="logo_project_bluebox_nav_item_left"><a id="expand_<?php echo $letter; ?>" class="active"><?php echo strtoupper($letter); ?></a></span>
+        <span class="logo_project_bluebox_nav_item_left"><a id="expand_<?php echo $letter; ?>"<?php echo ($letter == 'r') ? ' class="active"' : ''; ?>><?php echo strtoupper($letter); ?></a></span>
             <span class="seperator">|</span>
             <span class="logo_project_bluebox_nav_item_right"><a href="<?php echo get_permalink($logo_archive->ID); ?>?letter=<?php echo strtoupper($letter); ?>">View All</a></span>
         </span>
