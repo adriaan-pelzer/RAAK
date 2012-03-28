ed41e1e649c71fe1567a7814e95711abff2b3884
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-28 17:28:04 +0200

    creating default page.php file

---
+++ page.php
@@ -0,0 +1,8 @@
+<?php get_header() ?>
+
+    <div class="container rounded-corners blog">
+        <div class="content">
+        </div><!-- #content -->
+<?php get_sidebar() ?>
+<?php get_footer() ?>
+
