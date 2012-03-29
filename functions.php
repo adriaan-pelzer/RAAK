70d6c32a0e8f177b03d251356a19504673088314
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-29 12:18:17 +0200

    adding functionality for direct links to founder profiles to about page

--- functions.php
+++ functions.php
@@ -261,15 +261,11 @@ function who_we_are_what_we_do() {
     $current_uri = $_SERVER['REQUEST_URI'];
     $uri_array = explode('/', $current_uri);
     $founders = array('adriaan-pelzer', 'gerrie-smits', 'wessel-van-rensburg');
-    print_r($uri_array);
-    if(!in_array('about', $uri_array)) {
-        foreach($founders as $founder) {
-            if (in_array($founder, $uri_array)) {
-                $current_page = $founder;
-            }
+    $current_page = 'about';
+    foreach($founders as $founder) {
+        if (in_array($founder, $uri_array)) {
+            $current_page = $founder;
         }
-    } else {
-            $current_page = 'about';
     }
     echo $current_page;
     $what_we_do = get_page_by_title('What we do');
