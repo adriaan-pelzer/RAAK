7e53daa0907d9bc308a4d440e2c04ea42fcf8409
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-03-30 00:31:30 +0200

    fixing weird display issue

--- our_products.php
+++ our_products.php
@@ -12,6 +12,12 @@ Template Name: Our Products
 <?php get_sidebar() ?>
 <?php get_footer() ?>
 <script>
+if(!$('.whitebox_big_category_entry_content_picture').hasClass('current')) {
+    $('.whitebox_big_category_entry_content_picture').addClass('current');
+}
+if ($('.whitebox_big_category_entry_content_overview').hasClass('current')) {
+    $('.whitebox_big_category_entry_content_overview').removeClass('current');
+}
     $('.whitebox_big_category_entry_content').hover(function() {
         $(this).find('div').toggleClass('current');
     });
