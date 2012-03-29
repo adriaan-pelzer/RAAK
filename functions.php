7c37c36d1d23be75cc94021f0c498990f057cd6b
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-03-29 10:17:08 +0200

    making logo number dynamic, addding wp_reset_postdata() and replacing wp_reset_query() with wp_reset_postdata()

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ function logo_call_to_action() {
     $letters = array('r', 'a', 'k');
     foreach($letters as $letter) {
         $current_query = new WP_Query(array('post_type' => 'raak_logo_letter', 'meta_value' => $letter));
-        ${$letter . '_num'} = $current_query->post_count();
+        ${$letter . '_num'} = post_count();
     } 
     $logo_story = get_page_by_title('The perpetually changing crowdsourced RAAK logo', 'OBJECT', 'post');
     $logo_project = get_page_by_title('Logo Project');
