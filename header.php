1a56d60d5c46120f2f64870f32a032b03e627598
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-03-30 23:48:39 +0200

    dbugging contact form ajax

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
     <title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?><?php if ($wptitle == "") { echo " - "; bloginfo('description'); } ?></title>
     <meta name="google-site-verification" content="59Ab_0-HL7eVdNQ4CqiLOeiQisQgb2Vwg8046N__ng0" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <!-- script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script -->
+<?php wp_head(); ?>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script>
     var letters = [];
     </script>
@@ -39,7 +40,6 @@ foreach($different_letters as $different_letter) {
     <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-<?php wp_head(); ?>
 </head>
 <body class="wordpress y2011 m12 d07 h07  page pageid-4 page-author-admin page-template page-template-home-php">
 <div class="wrapper hfeed">
