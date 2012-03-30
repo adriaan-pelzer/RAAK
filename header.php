0870d27446b592c42b30a6f7660ecf5f815dd866
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-03-30 23:33:51 +0200

    dbugging contact form ajax

--- header.php
+++ header.php
@@ -31,7 +31,7 @@ foreach($different_letters as $different_letter) {
     }
 }
 ?>
-    <script src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/js/script.js"></script>
+    <!-- script src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/js/script.js"></script -->
     <link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style.css" />
     <!--[if lt IE 8]>
     <link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style_ie7.css" />
