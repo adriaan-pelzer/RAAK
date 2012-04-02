35c6e47b0dab055296a20ae3ef5f770adc8eed9c
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-04-02 11:00:55 +0200

    debug contact form ajax not working

--- header.php
+++ header.php
@@ -5,6 +5,34 @@
     <meta name="google-site-verification" content="59Ab_0-HL7eVdNQ4CqiLOeiQisQgb2Vwg8046N__ng0" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 <?php wp_head(); ?>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script>
+    var letters = [];
+    </script>
+<?php
+$different_letters = array('r', 'a', 'k');
+foreach($different_letters as $different_letter) {
+    $array_name = strtoupper($different_letter);
+?>
+    <script>
+    letters['<?php echo $array_name; ?>'] =[];
+    </script>
+<?php
+    $each_letter = new WP_Query(array('post_type' => 'raak_logo_letter', 'posts_per_page' => -1, 'meta_value' => $different_letter));
+    $i = 0;
+    while($each_letter->have_posts()) {
+        $each_letter->the_post();
+?>
+        <script>
+        //letters['<?php echo $array_name; ?>'][<?php echo $i; ?>] = '<?php echo get_post_meta(get_the_ID(), 'file', TRUE); ?>';
+        letters['<?php echo $array_name; ?>'].push('<?php echo get_post_meta(get_the_ID(), 'file', TRUE); ?>');
+        </script>
+<?php
+        $i++;
+    }
+}
+?>
+    <script src="http://stage.wewillraakyou.com/wp-content/themes/RAAK/js/script.js"></script>
     <link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style.css" />
     <!--[if lt IE 8]>
     <link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style_ie7.css" />
