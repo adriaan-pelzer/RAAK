01c1a97105880d2f8348937b8e657450d12328a8
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-04-03 13:00:43 +0200

    uploading a featured image

--- functions.php
+++ functions.php
@@ -621,7 +621,8 @@ function logo_project_upload_letter() {
                             add_post_meta($new_letter_id, 'file', $filename);
                             add_post_meta($new_letter_id, 'creatorip', (get_ip()));
                             add_post_meta($new_letter_id, 'originalname', $_FILES["upload_file"]["name"]);
-                            $image_id = insert_attachment($_FILES["upload_file"], $new_letter_id);
+                            $the_file = $_FILES['upload_file'];
+                            $image_id = insert_attachment($the_file, $new_letter_id);
                         }
                         $state = 2;
 
