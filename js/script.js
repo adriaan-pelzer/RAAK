7773f5520df1f2c578eded27ce1d381c1e64e49f
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-04-04 11:40:57 +0200

    using featured images in main logo

--- js/script.js
+++ js/script.js
@@ -56,8 +56,8 @@ var load_next = function() {
     $('#logo_letter_' + (letterNumber + 1) + ' span').animate({opacity: 0}, 1000, function() {
         $(this).html(letters[letter][variant]);
         //$(this).attr('src', 'http://stage.wewillraakyou.com/wp-content/themes/RAAK/resize.php?filename=logo_uploads/' + letters[letter][variant] + '&width=35&height=42');
-        $(this + ' img').load(function() {
-            $(this).animate({opacity: 1}, 1000);
+        $('#logo_letter_' + (letterNumber + 1) + ' span img').load(function() {
+            $('#logo_letter_' + (letterNumber + 1) + ' span').animate({opacity: 1}, 1000);
         });
     });
 }
