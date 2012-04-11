79e0f8e5790b821fc7f9655efa0546932f06df9b
Adriaan Pelzer <adriaan@Macintosh-2.local>
2012-04-11 11:33:36 +0200

    adding js to 404 and debugging permalinks for products

--- 404.php
+++ 404.php
@@ -6,5 +6,16 @@
 <?php do_shortcode('[other_posts category1="Must Read" category2="Worth a look" category3="RAAKonteur" colourscheme="blue" ]'); ?>
 <?php do_shortcode('[cat_box tab1="most-viewed" tab2="category" ]'); ?>
         </div><!-- #content -->
+<script>
+    $('.whitebox-secondary .multiple_tabs .tab').click(function() {
+        if (!$(this).hasClass('active')) {
+            $('.whitebox-secondary .multiple_tabs .tab.active').removeClass('active');
+            var currentID = $(this).attr('id');
+            $('.whitebox_secondary_item.current').removeClass('current');
+            $('#whitebox_secondary_item_' + currentID).addClass('current');
+            $(this).addClass('active');
+        }
+    });
+</script>
         <?php get_sidebar() ?>
 <?php get_footer() ?>
