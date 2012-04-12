0658b4c41fa9484e60c225cb883de689c46ca757
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-04-12 15:05:15 +0200

    working on transferring posts views

--- stats_table.php
+++ stats_table.php
@@ -578,7 +578,7 @@ for($i = 0; $i <= $posts_amount;$i++) {
     
 }
 print_r($posts_views);
-echo $posts_views[Klout is broken];
+echo $posts_views[1];
 $all_posts = new WP_Query(array('posts_per_page' => -1));
 foreach(($all_posts->posts) as $cur_post) {
     $key = $cur_post->post_title;
