a24ab07f1536282479771de1c76e937e0d3af147
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-04-12 14:59:43 +0200

    working on transferring posts views

--- stats_table.php
+++ stats_table.php
@@ -577,6 +577,7 @@ for($i = 0; $i <= $posts_amount;$i++) {
     $offset = stripos($stats_table, '</tr>', $second_title_offset);
     
 }
+print_r($posts_views);
 $all_posts = new WP_Query(array('posts_per_page' => -1));
 foreach(($all_posts->posts) as $cur_post) {
     $key = $cur_post->post_title;
