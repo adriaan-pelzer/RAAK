77376ed1c1fb0bcb775bbc8a49d3276fad51daa4
Adriaan Pelzer <adriaan@macintosh-2.mshome.net>
2012-04-12 12:12:09 +0200

    working on transferring posts views

--- stats_table.php
+++ stats_table.php
@@ -611,7 +611,7 @@ $offset = 0;
     $first_title_offset = stripos($stats_table, 'blank">', $initial_offset);
     $second_title_offset = stripos($stats_table, 'a>', $first_title_offset);
     $first_title_offset = $first_title_offset + 7;
-    $title_length = $second_title_offset - $first_titile_offset;
+    $title_length = $second_title_offset - $first_title_offset;
     $post_title = substr($stats_table, $first_title_offset, $title_length);
     echo '1st: ' . $first_title_offset;
     echo '2nd: ' . $second_title_offset;
