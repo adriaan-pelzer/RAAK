226d57a217b6dbbea26485e54d3ad2a2ccbed185
Simon Kruger <nimosza@gmail.com>
2012-04-19 12:39:09 +0200

    moving definition of letter array to external file to facilitate minification

--- header.php
+++ header.php
@@ -13,33 +13,7 @@
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script>
-    var is_logo_page = '<?php echo (is_page_template('logo_project.php') ? 'yes' : 'no'); ?>';
-    var letters = [];
-    </script>
-<?php
-$different_letters = array('r', 'a', 'k');
-foreach($different_letters as $different_letter) {
-    $array_name = strtoupper($different_letter);
-?>
-    <script>
-    letters['<?php echo $array_name; ?>'] =[];
-    </script>
-<?php
-    $each_letter = new WP_Query(array('post_type' => 'raak_logo_letter', 'posts_per_page' => -1, 'meta_value' => $different_letter));
-    $i = 0;
-    while($each_letter->have_posts()) {
-        $each_letter->the_post();
-?>
-        <script>
-        //letters['<?php echo $array_name; ?>'][<?php echo $i; ?>] = '<?php echo get_post_meta(get_the_ID(), 'file', TRUE); ?>';
-        letters['<?php echo $array_name; ?>'].push('<?php echo get_the_post_thumbnail(get_the_ID()); ?>');
-        </script>
-<?php
-        $i++;
-    }
-}
-?>
+<?php include 'letter_array_create.php'; ?>
 <?php wp_head(); ?>
     <script src="<?php bloginfo ("template_url"); ?>/js/script.js"></script>
 <!--script type="text/javascript" src="/min/f=wp-content/themes/RAAK/js/script.js"></script-->
---
+++ letter_array_create.php
@@ -0,0 +1,27 @@
+<script>
+    var is_logo_page = '<?php echo (is_page_template('logo_project.php') ? 'yes' : 'no'); ?>';
+    var letters = [];
+    </script>
+<?php
+$different_letters = array('r', 'a', 'k');
+foreach($different_letters as $different_letter) {
+    $array_name = strtoupper($different_letter);
+?>
+    <script>
+    letters['<?php echo $array_name; ?>'] =[];
+    </script>
+<?php
+    $each_letter = new WP_Query(array('post_type' => 'raak_logo_letter', 'posts_per_page' => -1, 'meta_value' => $different_letter));
+    $i = 0;
+    while($each_letter->have_posts()) {
+        $each_letter->the_post();
+?>
+        <script>
+        //letters['<?php echo $array_name; ?>'][<?php echo $i; ?>] = '<?php echo get_post_meta(get_the_ID(), 'file', TRUE); ?>';
+        letters['<?php echo $array_name; ?>'].push('<?php echo get_the_post_thumbnail(get_the_ID()); ?>');
+        </script>
+<?php
+        $i++;
+    }
+}
+?>
