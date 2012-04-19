daca31f0694c732cb5905927a21f753ab8f9c282
Simon Kruger <nimosza@gmail.com>
2012-04-19 12:09:52 +0200

    removing minify

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
     <title><?php wp_title( '-', true, 'right' ); echo wp_specialchars( get_bloginfo('name'), 1 ) ?><?php if ($wptitle == "") { echo " - "; bloginfo('description'); } ?></title>
     <meta name="google-site-verification" content="59Ab_0-HL7eVdNQ4CqiLOeiQisQgb2Vwg8046N__ng0" />
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <!-- link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style.css" / -->
-<link type="text/css" rel="stylesheet" href="/min/f=wp-content/themes/RAAK/css/style.css" />
+    <link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style.css" />
+<!-- link type="text/css" rel="stylesheet" href="/min/f=wp-content/themes/RAAK/css/style.css" /-->
     <!--[if lt IE 8]>
     <link rel="stylesheet" href="<?php bloginfo ("template_url"); ?>/css/style_ie7.css" />
     <![endif]-->
@@ -41,8 +41,8 @@ foreach($different_letters as $different_letter) {
     }
 }
 ?>
-    <!-- script src="<?php bloginfo ("template_url"); ?>/js/script.js"></script -->
-<script type="text/javascript" src="/min/f=wp-content/themes/RAAK/js/script.js"></script>
+    <script src="<?php bloginfo ("template_url"); ?>/js/script.js"></script>
+<!--script type="text/javascript" src="/min/f=wp-content/themes/RAAK/js/script.js"></script-->
 </head>
 <body class="wordpress y2011 m12 d07 h07  page pageid-4 page-author-admin page-template page-template-home-php">
 <div class="wrapper hfeed">
