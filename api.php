48fdc097e522ea805c4632b88c022c34b876e139
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2012-07-06 17:41:03 +0100

    Mailchimp API integration

--- api.php
+++ api.php
@@ -3,6 +3,32 @@
 Template Name: API
  */
 
+function return_json ($arr) {
+    header('Cache-Control: no-cache, must-revalidate');
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+    header('Content-type: application/json');
+    echo json_encode ($arr);
+    die ();
+}
+
+/*if (!empty($_GET['apikey'])) {
+    return_json(array('code' => -1, 'error' => 'Please specify a mailchimp API key'));
+} else {
+    $apikey = $_GET['apikey'];
+}
+
+if (!empty($_GET['listid'])) {
+    return_json(array('code' => -1, 'error' => 'Please specify a mailchimp list id'));
+} else {
+    $listid = $_GET['listid'];
+}
+
+if (!empty($_GET['subject'])) {
+    return_json(array('code' => -1, 'error' => 'Please specify a mailchimp subject'));
+} else {
+    $apikey = $_GET['subject'];
+}*/
+
 $api = create_mc();
 
 $lists = get_lists($api);
@@ -10,4 +36,8 @@ $lists = get_lists($api);
 for ($i = 0; $i < $lists['total']; $i++) {
     echo $lists['data'][$i]['id']." - ".$lists['data'][$i]['name']."<br />\n";
 }
+
+$retval = create_mc_campaign($api, '9b809ef490', 'Test letter', 'Test letter title', '<h2 class="subTitle">Hallo hallo</h2>');
+
+echo "Return value: ".$retval;
 ?>
