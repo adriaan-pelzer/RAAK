5d103342414828b81a9a97769162b1e1369ccb3d
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2012-07-06 18:19:29 +0100

    Mailchimp API integration

--- api.php
+++ api.php
@@ -50,7 +50,7 @@ foreach (array('id', 'web_id', 'folder_id', 'create_time', 'send_time', 'status'
 $campaign['title'] = "Test title";
 $campaign['subject'] = "Test subject";
 
-$retval = $api->campaignCreate('regular', $campaign, array('html' => "<h2 class=\"subTitle\">test Title</h2>"));
+$retval = $api->campaignCreate('regular', $campaign, array('html' => "<h2 class=\"subTitle\">test Title</h2>", 'text' => "test Title"));
 
 if (!$retval) {
     print_r($api);
