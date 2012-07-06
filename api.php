066487e229afba122282009e780a6a6c55c5adec
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2012-07-06 17:15:38 +0100

    Mailchimp API integration

--- api.php
+++ api.php
@@ -8,6 +8,6 @@ $api = create_mc();
 $lists = get_lists($api);
 
 for ($i = 0; $i < $lists['total']; $i++) {
-    print_r($lists['data'][$i]);
+    echo $lists['data'][$i]['id']." - ".$lists['data'][$i]['name']."<br />\n";
 }
 ?>
