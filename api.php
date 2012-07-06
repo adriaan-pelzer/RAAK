3cb72eb2ced21d4c26cea8b1bf0962cf16233c7c
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2012-07-06 16:59:49 +0100

    Mailchimp API integration

--- api.php
+++ api.php
@@ -2,15 +2,13 @@
 /*
 Template Name: API
  */
-get_header();
 
 $api = create_mc();
 
 $lists = get_lists($api);
 
 echo "Lists\n";
-//print_r($lists);
+print_r($lists);
 echo "API\n";
-//print_r($api);
-get_footer();
+print_r($api);
 ?>
